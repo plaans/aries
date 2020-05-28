631ce6206a83d6838e82e6b5e300bdd9fae25903
Arthur Bit-Monnot <abitmonnot@laas.fr>
2020-05-28 15:47:56 +0200

    Add comments in plan_search.

--- src/planning/classical/search.rs
+++ src/planning/classical/search.rs
@@ -35,6 +35,7 @@ pub fn plan_search(initial_state: &State, ops: &Operators, goals: &[Lit]) -> Opt
     let mut heap = BinaryHeap::new();
     let mut closed = HashSet::new();
 
+    // initialize the priority queue with the initiale state
     let init = Node {
         s: initial_state.clone(),
         plan: Vec::new(),
@@ -42,24 +43,38 @@ pub fn plan_search(initial_state: &State, ops: &Operators, goals: &[Lit]) -> Opt
     };
     heap.push(init);
 
+    // keep expanding the search tree until the priority queue is empty
     while let Some(n) = heap.pop() {
         if closed.contains(&n.s) {
+            // we already visited this state, skip it.
             continue;
         }
+
+        // note that we are visiting this state to avoid going back to it
         closed.insert(n.s.clone());
+
+        // compute hadd to get the set of applicable actions
+        // TODO: there is a lot of costly unnecessary work here
         let hres = hadd(&n.s, ops);
+
+        // for each operator applicable in the current state
         for &op in hres.applicable_operators() {
             debug_assert!(n.s.entails_all(ops.preconditions(op)));
+
+            // clone the state and apply effects
             let mut s = n.s.clone();
             s.set_all(ops.effects(op));
 
+            // create the corresponding plan
             let mut plan = n.plan.clone();
             plan.push(op);
 
+            // check if we have reached a goal state
             if s.entails_all(goals) {
                 return Some(plan);
             }
 
+            // compute heuristic value and add to queue
             let hres = hadd(&s, ops);
             let f = (plan.len() as Cost) + 3 * hres.conjunction_cost(goals);
             let succ = Node { s, plan, f };
@@ -67,5 +82,6 @@ pub fn plan_search(initial_state: &State, ops: &Operators, goals: &[Lit]) -> Opt
         }
     }
 
+    // we have exausted the search space without finding a goal state, problem in unsolvable
     None
 }
