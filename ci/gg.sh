d10d8842e9eab1a30474fa2be0cf4c73bc188fcc
Arthur Bit-Monnot <abitmonnot@laas.fr>
2020-06-15 12:41:09 +0200

    [ci] Add problem solving with GG to github actions.

--- ci/gg.sh
+++ ci/gg.sh
@@ -3,4 +3,4 @@
 set -e
 
 GG=./target/release/gg TIMEOUT=30s ./ci/gg-solve-all.sh ci/gg-problems-release.txt
-GG=./target/debug/gg TIMEOUT=2m ./ci/gg-solve-all.sh ci/gg-problems-debug.txt
+GG=./target/debug/gg TIMEOUT=3m ./ci/gg-solve-all.sh ci/gg-problems-debug.txt
