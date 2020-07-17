f651dec3a1e3f5260db6ea8ed48ed327dd3ba3b4
Arthur Bit-Monnot <abitmonnot@laas.fr>
2020-07-17 17:37:37 +0200

    [CI] set up automated testing of sat solver.

--- ci/minisat.sh
+++ ci/minisat.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-cargo test
-
-cargo build --release
+#cargo build --bin minisat
+#EXE="./target/debug/minisat"
+cargo build --release --bin minisat
 EXE="./target/release/minisat"
+
+export RUST_BACKTRACE=1
 
 for filename in problems/cnf/sat/*.cnf; do
     echo "Running SAT: $filename"
