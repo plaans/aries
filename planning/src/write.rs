00e058f94565b0cefa2ea144be639beec19ded35
Bastien <bastienjoblot@gmail.com>
2020-09-22 13:41:11 +0200

    format

--- planning/src/write.rs
+++ planning/src/write.rs
@@ -1,22 +1,18 @@
-use std::fs::File;
 use crate::classical::state::*;
 use crate::classical::GroundProblem;
-use crate::symbols::{SymbolTable};
+use crate::symbols::SymbolTable;
+use std::fs::File;
 use std::io::Write;
 
-
-pub fn writeplan(path: String,plan: &[Op],ground: &GroundProblem,symb: &SymbolTable<String, String>){
+pub fn writeplan(path: String, plan: &[Op], ground: &GroundProblem, symb: &SymbolTable<String, String>) {
     // let path = "graphique.dot";
 
     //let path = path.split_whitespace();
-    let mut copypath=path;
+    let mut copypath = path;
     copypath.pop();
-     let mut output = File::create(copypath).expect("Something went wrong reading the file");
- 
-     for &op in plan {
-         writeln!(output, "{}",symb.format(ground.operators.name(op)))
-                         .expect("Something went wrong writing the file");
-     }
- 
- }
- 
+    let mut output = File::create(copypath).expect("Something went wrong reading the file");
+
+    for &op in plan {
+        writeln!(output, "{}", symb.format(ground.operators.name(op))).expect("Something went wrong writing the file");
+    }
+}
