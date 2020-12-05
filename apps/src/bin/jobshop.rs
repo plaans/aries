c2005387aed08b0f593c256479fcab682de60e37
Arthur Bit-Monnot <abitmonnot@laas.fr>
2020-12-05 13:57:02 +0100

    [jobshop] Clean up implementation from outdated code.

--- apps/src/bin/jobshop.rs
+++ apps/src/bin/jobshop.rs
@@ -113,77 +113,11 @@ fn main() {
     if let Some((optimum, solution)) = result {
         println!("Found optimal solution with makespan: {}", optimum);
         assert_eq!(solution.lower_bound(makespan), optimum);
-        for v in solver.model.ints.variables() {
-            println!("{} <- {}", solver.model.fmt(v), solution.lower_bound(v));
-        }
         println!("{}", solver.stats)
     } else {
         eprintln!("NO SOLUTION");
     }
-    println!("RUNTIME: {:.6}", start_time.elapsed().as_secs_f64());
-
-    // let (mut smt, makespan_var) = init_jobshop_solver(&pb, opt.upper_bound);
-    // let x = smt.theory.propagate_all();
-    // assert_eq!(x, NetworkStatus::Consistent);
-    // let lower_bound = (opt.lower_bound as i32)
-    //     .max(smt.theory.lb(makespan_var))
-    //     .max(pb.makespan_lower_bound());
-    // println!("Initial lower bound: {}", lower_bound);
-    //
-    // // find initial solution
-    // let mut lvl = smt.theory.set_backtrack_point();
-    // smt.solve(use_lazy);
-    // let mut makespan = smt.theory.lb(makespan_var);
-    // println!("Found initial solution.\nMakespan: {}", makespan);
-    //
-    // let optimal_makespan = loop {
-    //     smt.theory.backtrack_to(lvl);
-    //     // TODO: allow the addition of persistent constraints to a theory to avoid the need to backtrack
-    //     //       all the way to the ground
-    //     smt.sat.backtrack_to(DecisionLevel::GROUND);
-    //     smt.theory.add_edge(smt.theory.origin(), makespan_var, makespan - 1);
-    //     match smt.theory.propagate_all() {
-    //         NetworkStatus::Consistent => (),
-    //         NetworkStatus::Inconsistent(_) => {
-    //             break makespan;
-    //         }
-    //     }
-    //     lvl = smt.theory.set_backtrack_point();
-    //     match smt.solve(use_lazy) {
-    //         Some(_model) => {
-    //             makespan = smt.theory.lb(makespan_var);
-    //             println!("Improved makespan: {}", makespan);
-    //             if use_lns {
-    //                 // Mimic Large-Neighborhood Search (LNS) behavior :
-    //                 // The polarity (i.e. preferred value) of each variable is set to the value
-    //                 // it takes in the best solution.
-    //                 // This will make the solver explore variations of the current solution in an
-    //                 // attempt to improve it.
-    //                 for var in smt.sat.variables() {
-    //                     match smt.sat.get_variable(var) {
-    //                         Some(x) => smt.sat.set_polarity(var, x),
-    //                         None => unreachable!("All variables should have been set."),
-    //                     }
-    //                 }
-    //             }
-    //             assert!(makespan >= lower_bound);
-    //             if makespan == lower_bound {
-    //                 break makespan;
-    //             }
-    //         }
-    //         None => {
-    //             break makespan;
-    //         }
-    //     }
-    // };
-    // println!("Optimal solution found: {}", optimal_makespan);
-    // println!("{}", smt.sat.stats);
-    // if let Some(target) = opt.expected_makespan {
-    //     if optimal_makespan != target as i32 {
-    //         eprintln!("Error: expected an optimal makespan of {}", target);
-    //         std::process::exit(1);
-    //     }
-    // }
+    println!("TOTAL RUNTIME: {:.6}", start_time.elapsed().as_secs_f64());
 }
 
 fn parse(input: &str) -> JobShop {
