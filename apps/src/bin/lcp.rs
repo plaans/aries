af7ff3554cb14f780c95af8267f6fb444223be5d
Yirmandias <turi@etud.insa-toulouse.fr>
2020-12-17 17:16:17 +0100

    [lcp] add simetry breaking

--- apps/src/bin/lcp.rs
+++ apps/src/bin/lcp.rs
@@ -10,6 +10,7 @@ use aries_planning::chronicles::constraints::ConstraintType;
 use aries_sat::all::Lit;
 use aries_sat::SatProblem;
 
+use crate::SymetryBreakingTpe::{ADVANCED, SIMPLE};
 use aries_model::assignments::{Assignment, SavedAssignment};
 use aries_model::lang::{Atom, BAtom, BVar, IAtom, IVar, Variable};
 use aries_model::symbols::SymId;
@@ -23,6 +24,7 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 use structopt::StructOpt;
+//use std::intrinsics::caller_location;
 
 /// Generates chronicles from a PDDL problem specification.
 #[derive(Debug, StructOpt)]
@@ -197,6 +199,65 @@ fn conditions(pb: &FiniteProblem) -> impl Iterator<Item = (BAtom, &Condition)> {
 const ORIGIN: i32 = 0;
 const HORIZON: i32 = 999999;
 
+enum SymetryBreakingTpe {
+    SIMPLE,
+    ADVANCED,
+}
+
+fn add_simetry_breaking(
+    pb: &FiniteProblem,
+    model: &mut Model,
+    constraints: &mut Vec<BAtom>,
+    tpe: SymetryBreakingTpe,
+) -> Result<()> {
+    match tpe {
+        SIMPLE => {
+            for (instance1, origin1) in pb
+                .chronicles
+                .iter()
+                .filter(|&c| c.origin != ChronicleOrigin::Original)
+                .map(|c| {
+                    (
+                        c,
+                        match c.origin {
+                            ChronicleOrigin::Instantiated(v) => Some(v),
+                            _ => None,
+                        }
+                        .unwrap(),
+                    )
+                })
+            {
+                for (instance2, origin2) in pb
+                    .chronicles
+                    .iter()
+                    .filter(|&c| c.origin != ChronicleOrigin::Original)
+                    .map(|c| {
+                        (
+                            c,
+                            match c.origin {
+                                ChronicleOrigin::Instantiated(v) => Some(v),
+                                _ => None,
+                            }
+                            .unwrap(),
+                        )
+                    })
+                {
+                    if origin1.template_id == origin2.template_id && origin1.instantiation_id < origin2.instantiation_id
+                    {
+                        constraints.push(model.implies(instance1.chronicle.presence, instance2.chronicle.presence));
+                        constraints.push(model.leq(instance1.chronicle.start, instance2.chronicle.start))
+                    }
+                }
+            }
+        }
+        ADVANCED => {
+            //TODO:Implement advanced simetry breaking
+        }
+    };
+
+    Ok(())
+}
+
 fn encode(pb: &FiniteProblem) -> anyhow::Result<(Model, Vec<BAtom>)> {
     let mut model = pb.model.clone();
 
@@ -331,6 +392,8 @@ fn encode(pb: &FiniteProblem) -> anyhow::Result<(Model, Vec<BAtom>)> {
             }
         }
     }
+
+    add_simetry_breaking(pb, &mut model, &mut constraints, SIMPLE)?;
 
     Ok((model, constraints))
 }
