6ba6adce5c092871c55a242942c8b808cd5faa22
Arthur Bit-Monnot <abitmonnot@laas.fr>
2021-02-16 18:10:10 +0100

    [model] Add domains write benchmark.

--- model/benches/domains.rs
+++ model/benches/domains.rs
@@ -1,7 +1,9 @@
+use aries_backtrack::Backtrack;
 use aries_model::assignments::Assignment;
 use aries_model::bounds::Bound;
+use aries_model::int_model::domains::Domains;
 use aries_model::lang::IVar;
-use aries_model::Model;
+use aries_model::{Model, WriterId};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::prelude::{SliceRandom, StdRng};
 use rand::{Rng, SeedableRng};
@@ -36,7 +38,7 @@ fn count_upper_bounds(xs: &[IVar], model: &Model) -> usize {
     count
 }
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn read_benchmark(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(2398248538438434234);
 
     let mut model = Model::new();
@@ -70,6 +72,69 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn enforce_all(model: &mut Domains, lits: &[(Bound, u32)]) {
+    let writer = WriterId::new(1u8);
+
+    for &(lit, cause) in lits {
+        model.set(lit, writer.cause(cause)).unwrap();
+    }
+}
+
+fn backtrack_full(model: &mut Domains) {
+    model.reset()
+}
+
+pub fn write_benchmark(c: &mut Criterion) {
+    let mut rng = StdRng::seed_from_u64(2398248538438434234);
+
+    let mut model = Model::new();
+
+    let mut variables = Vec::new();
+    let mut literals = Vec::new();
+
+    for _ in 0..1000 {
+        let var = model.new_ivar(0, 1000, "");
+        variables.push(var);
+    }
+
+    for i in 10..400 {
+        variables.shuffle(&mut rng);
+        for v in &variables {
+            let min_update = Bound::geq(*v, 10 + rng.gen_range((i - 2)..(i + 2)));
+            let max_update = Bound::leq(*v, 990 - rng.gen_range((i - 2)..(i + 2)));
+            let i = i as u32;
+            if rng.gen_bool(0.5) {
+                literals.push((min_update, i));
+                literals.push((max_update, i));
+            } else {
+                literals.push((max_update, i));
+                literals.push((min_update, i));
+            }
+        }
+    }
+
+    literals.shuffle(&mut rng);
+    variables.shuffle(&mut rng);
+
+    c.bench_function("model-writes-no-error", |b| {
+        b.iter(|| {
+            let dom = &mut model.discrete.domains.clone();
+            enforce_all(black_box(dom), black_box(&literals))
+        });
+    });
+
+    enforce_all(&mut model.discrete.domains, &literals);
+    let base = model.discrete.domains;
+    c.bench_function("model-backtrack", |b| {
+        b.iter(|| backtrack_full(black_box(&mut base.clone())));
+    });
+}
+
+criterion_group!(
+    benches,
+    // read_benchmark,
+    write_benchmark,
+);
+// criterion_group!(benches, write_benchmark);
 
 criterion_main!(benches);
