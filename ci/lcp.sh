82c14c1e9850b05db33495304ff939c03af9f7be
Arthur Bit-Monnot <abitmonnot@laas.fr>
2021-05-12 14:17:13 +0200

    [ci] Limit memory usage in planning CI

--- ci/lcp.sh
+++ ci/lcp.sh
@@ -1,7 +1,7 @@
 
 set -e # Exit on first error
 
-# Path TO GG (defaults to debug build)
+# Path TO planner and validators (defaults to release build)
 PLANNER="${PLANNER:-target/release/lcp}"
 HDDL_VAL="${HDDL_VAL:-ext/pandaPIparser}"
 PDDL_VAL="${PDDL_VAL:-ext/validate}"
@@ -21,7 +21,7 @@ HDDL_PROBLEMS=$(find problems/hddl -name instance-1.hddl)
 
 for PROB_FILE in $HDDL_PROBLEMS
 do
-    DOM_FILE="$(dirname $PROB_FILE)/domain.hddl"
+    DOM_FILE="$(dirname "$PROB_FILE")/domain.hddl"
     PLAN_FILE=$(mktemp)
     COMMAND="timeout ${TIMEOUT} ${PLANNER} ${PROB_FILE} -o ${PLAN_FILE} &&  ${HDDL_VAL} -l -verify ${DOM_FILE} ${PROB_FILE} ${PLAN_FILE}"
 
@@ -34,15 +34,20 @@ PDDL_PROBLEMS=$(find problems/pddl -name instance-1.pddl)
 
 for PROB_FILE in $PDDL_PROBLEMS
 do
-    DOM_FILE="$(dirname $PROB_FILE)/domain.pddl"
+    DOM_FILE="$(dirname "$PROB_FILE")/domain.pddl"
     PLAN_FILE=$(mktemp)
     COMMAND="timeout ${TIMEOUT} ${PLANNER} ${PROB_FILE} -o ${PLAN_FILE} &&  ${PDDL_VAL} ${DOM_FILE} ${PROB_FILE} ${PLAN_FILE}"
 
-    echo $COMMAND >> $COMMANDS
+    echo "$COMMAND" >> "$COMMANDS"
 done
 
+
+# limit (global?) memory usage to 1GB
+ulimit -m 1000000
+ulimit -v 1000000
+
 # run all commands in parallel
-cat $COMMANDS | parallel -v --halt-on-error now,fail=1 '{}'
+cat "$COMMANDS" | parallel -v --halt-on-error now,fail=1 '{}'
 
 echo "======== Successful runs ======="
 cat "$COMMANDS"
