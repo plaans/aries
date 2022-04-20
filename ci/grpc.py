51286ed004128f9ea92d91716147756f46ce5525
franklinselva <franklinselva10@gmail.com>
2022-04-20 18:10:29 +0200

    ci: add `grpc` checker to ci

---
+++ ci/grpc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Script that should be run from the root of the repository.
+# It validates that the GRPC server with a set of binaries from the UPF platform
+
+import os
+import subprocess
+
+os.system("cargo build --profile ci --bin up-server")
+solver = "target/ci/up-server"
+
+solver_cmd = solver + " {instance}"
+
+instances = [
+    "./ext/up/bins/problems/matchcellar.bin",
+    "./ext/up/bins/problems/robot_fluent_of_user_type.bin",
+]
+# instances = []
+# for instance in os.listdir("./ext/grpc/bins"):
+#     if instance.endswith(".bin"):
+#         instances.append(os.path.join("./ext/grpc/bins", instance))
+# TODO: Safely handle unsupported instances based on the answer status
+# TODOL: Add support for parallel execution
+for instance in instances:
+    cmd = solver_cmd.format(instance=instance).split(" ")
+    print("Solving instance: " + instance)
+    print("Command: " + " ".join(cmd))
+    solver_run = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    if solver_run.returncode != 0:
+        print("Solver did not return expected result")
+        exit(1)
