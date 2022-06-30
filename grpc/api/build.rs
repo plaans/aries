12c803b10cfca52a7934e3ff6c6f4f5d23636375
Arthur Bit-Monnot <abitmonnot@laas.fr>
2022-06-30 14:58:53 +0200

    [proto] Clean up protobuf build script.

--- grpc/api/build.rs
+++ grpc/api/build.rs
@@ -2,28 +2,19 @@ use std::fs;
 
 //Build GRPC server and client for UPF planning service
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_files = &["src/unified_planning.proto"];
+    let proto_file = "src/unified_planning.proto";
 
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .out_dir("src/")
-        .compile(proto_files, &["src/"])
+        .compile(&[proto_file], &["src/"])
         .unwrap_or_else(|e| panic!("Failed to compile proto: {}", e));
 
-    // Recompile only if the proto file has been modified
-    for file in proto_files {
-        println!("cargo:rerun-if-changed={}", file);
-    }
+    fs::rename("src/_.rs", "src/unified_planning.rs")?;
 
-    // Rename the generated files to match the proto file names
-    // FIXME: This is a hack, and should be fixed in the future
-    for file in fs::read_dir("src/")? {
-        let file = file?;
-        let path = file.path();
-        let file_name = path.file_name().unwrap().to_str().unwrap();
-        let new_file_name = file_name.replace("_.rs", "unified_planning.rs");
-        fs::rename(path.clone(), path.with_file_name(new_file_name))?;
-    }
+    // Recompile only if the proto file has been modified
+    println!("cargo:rerun-if-changed={}", proto_file);
+
     Ok(())
 }
