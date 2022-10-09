41c29789a0d59062b5431489ad950ccb95f62562
Arthur Bit-Monnot <abitmonnot@laas.fr>
2022-10-09 14:08:14 +0200

    [sched] Initial shot an openshop scheduler.

--- examples/scheduling/bin/openshop.rs
+++ examples/scheduling/bin/openshop.rs
@@ -1,65 +1,35 @@
+use anyhow::*;
 use aries_backtrack::{Backtrack, DecLvl};
 use aries_core::*;
 use aries_model::extensions::{AssignmentExt, Shaped};
 use aries_model::lang::expr::leq;
 use aries_model::lang::IVar;
-use aries_solver::solver::search::activity::{ActivityBrancher, Heuristic};
+use aries_scheduling::*;
+use aries_solver::solver::search::activity::ActivityBrancher;
 use aries_solver::solver::search::{Decision, SearchControl};
 use aries_solver::solver::stats::Stats;
 use aries_stn::theory::{StnConfig, StnTheory};
 use std::fmt::Write;
 use std::fs;
-use std::str::FromStr;
 use structopt::StructOpt;
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-pub enum Var {
-    /// Variable representing the makespan (constrained to be after the end of tasks
-    Makespan,
-    /// Variable representing the start time of (job_number, task_number_in_job)
-    Start(usize, usize),
-    Prec(usize, usize, usize, usize),
-}
-
-impl std::fmt::Display for Var {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-type Model = aries_model::Model<Var>;
-type Solver = aries_solver::solver::Solver<Var>;
-type ParSolver = aries_solver::parallel_solver::ParSolver<Var>;
+use walkdir::WalkDir;
 
 #[derive(Clone, Debug)]
-struct JobShop {
+struct OpenShop {
     pub num_jobs: usize,
     pub num_machines: usize,
     times: Vec<i32>,
-    machines: Vec<usize>,
 }
 
-impl JobShop {
+impl OpenShop {
     pub fn duration(&self, job: usize, op: usize) -> i32 {
         self.times[job * self.num_machines + op]
     }
     pub fn machines(&self) -> impl Iterator<Item = usize> {
-        1..=self.num_machines
+        0..self.num_machines
     }
     pub fn jobs(&self) -> impl Iterator<Item = usize> {
         0..self.num_jobs
-    }
-
-    pub fn machine(&self, job: usize, op: usize) -> usize {
-        self.machines[job * self.num_machines + op]
-    }
-    pub fn op_with_machine(&self, job: usize, machine: usize) -> usize {
-        for i in 0..self.num_machines {
-            if self.machine(job, i) == machine {
-                return i;
-            }
-        }
-        panic!("This job is missing a machine")
     }
 
     /// Computes a lower bound on the makespan as the maximum of the operation durations in each
@@ -70,12 +40,8 @@ impl JobShop {
             .max()
             .unwrap();
 
-        let max_by_machine: i32 = (1..self.num_machines + 1)
-            .map(|m| {
-                (0..self.num_jobs)
-                    .map(|job| self.duration(job, self.op_with_machine(job, m)))
-                    .sum()
-            })
+        let max_by_machine: i32 = (0..self.num_machines)
+            .map(|m| (0..self.num_jobs).map(|job| self.duration(job, m)).sum())
             .max()
             .unwrap();
 
@@ -103,33 +69,28 @@ struct Opt {
     search: SearchStrategy,
 }
 
-/// Search strategies that can be added to the solver.
-#[derive(Eq, PartialEq, Debug)]
-enum SearchStrategy {
-    /// Activity based search
-    Activity,
-    /// Variable selection based on earliest starting time + least slack
-    Est,
-    /// Run both Activity and Est in parallel.
-    Parallel,
-}
-impl FromStr for SearchStrategy {
-    type Err = String;
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "act" | "activity" => Ok(SearchStrategy::Activity),
-            "est" => Ok(SearchStrategy::Est),
-            "par" | "parallel" => Ok(SearchStrategy::Parallel),
-            e => Err(format!("Unrecognized option: '{}'", e)),
+    let file = &opt.file;
+    if std::fs::metadata(file)?.is_file() {
+        solve(&opt.file, &opt);
+        Ok(())
+    } else {
+        for entry in WalkDir::new(file).follow_links(true).into_iter().filter_map(|e| e.ok()) {
+            let f_name = entry.file_name().to_string_lossy();
+            if f_name.ends_with(".txt") {
+                println!("{}", f_name);
+                solve(&entry.path().to_string_lossy(), &opt);
+            }
         }
+        Ok(())
     }
 }
 
-fn main() {
+fn solve(filename: &str, opt: &Opt) {
     let start_time = std::time::Instant::now();
-    let opt = Opt::from_args();
-    let filecontent = fs::read_to_string(opt.file).expect("Cannot read file");
+    let filecontent = fs::read_to_string(filename).expect("Cannot read file");
 
     let pb = parse(&filecontent);
 
@@ -166,11 +127,10 @@ fn main() {
             // all tasks on this machine
             let mut tasks = Vec::new();
             for j in 0..pb.num_jobs {
-                let op = pb.op_with_machine(j, m);
-                let task = Var::Start(j, op);
+                let task = Var::Start(j, m);
                 let start_var = solver.get_int_var(&task).unwrap();
                 let start_time = solution.var_domain(start_var).lb;
-                tasks.push(((j, op), start_time));
+                tasks.push(((j, m), start_time));
             }
             // sort task by their start time
             tasks.sort_by_key(|(_task, start_time)| *start_time);
@@ -197,6 +157,7 @@ fn main() {
                 optimum, expected
             );
         }
+        println!("XX\t{}\t{}\t{}", filename, optimum, start_time.elapsed().as_secs_f64());
     } else {
         eprintln!("NO SOLUTION");
         assert!(opt.expected_makespan.is_none(), "Expected a valid solution");
@@ -204,37 +165,48 @@ fn main() {
     println!("TOTAL RUNTIME: {:.6}", start_time.elapsed().as_secs_f64());
 }
 
-fn parse(input: &str) -> JobShop {
-    let mut lines = input.lines();
-    lines.next(); // drop header "num_jobs num_machines"
-    let x: Vec<&str> = lines.next().unwrap().split_whitespace().collect();
-    let num_jobs = x[0].parse().unwrap();
-    let num_machines = x[1].parse().unwrap();
+fn is_comment(line: &str) -> bool {
+    line.chars().any(|c| c == '#')
+    // !line.chars().all(|c| c.is_whitespace() || c.is_numeric())
+}
 
-    lines.next(); // drop "Times" line
+fn ints(input_line: &str) -> impl Iterator<Item = usize> + '_ {
+    input_line.split_whitespace().map(|n| n.parse().unwrap())
+}
+
+fn lines(input: &str) -> impl Iterator<Item = &str> + '_ {
+    input.lines().filter(|l| !is_comment(*l))
+}
+
+fn parse(input: &str) -> OpenShop {
+    let mut lines = lines(input);
+    //input.lines().peekable();
+    // if is_comment(lines.peek().unwrap()) {
+    //     lines.next(); // drop "Times" line
+    // }
+    lines.next();
+    let mut x = ints(lines.next().unwrap());
+    let num_jobs = x.next().unwrap();
+    let num_machines = x.next().unwrap();
+
+    // if is_comment(lines.peek().unwrap()) {
+    //     lines.next(); // drop "Times" line
+    // }
     let mut times = Vec::with_capacity(num_machines * num_jobs);
     for _ in 0..num_jobs {
-        for t in lines.next().unwrap().split_whitespace() {
-            times.push(t.parse().unwrap())
-        }
-    }
-    lines.next(); // drop "Machines" line
-    let mut machines = Vec::with_capacity(num_machines * num_jobs);
-    for _ in 0..num_jobs {
-        for t in lines.next().unwrap().split_whitespace() {
-            machines.push(t.parse().unwrap())
+        for t in ints(lines.next().unwrap()) {
+            times.push(t as i32)
         }
     }
 
-    JobShop {
+    OpenShop {
         num_jobs,
         num_machines,
         times,
-        machines,
     }
 }
 
-fn encode(pb: &JobShop, lower_bound: u32, upper_bound: u32) -> Model {
+fn encode(pb: &OpenShop, lower_bound: u32, upper_bound: u32) -> Model {
     let start = |model: &Model, j: usize, t: usize| IVar::new(model.shape.get_variable(&Var::Start(j, t)).unwrap());
     let end = |model: &Model, j: usize, t: usize| start(model, j, t) + pb.duration(j, t);
 
@@ -244,47 +216,36 @@ fn encode(pb: &JobShop, lower_bound: u32, upper_bound: u32) -> Model {
 
     let makespan_variable = m.new_ivar(lower_bound, upper_bound, Var::Makespan);
     for j in 0..pb.num_jobs {
-        for i in 0..pb.num_machines {
-            let task_start = m.new_ivar(0, upper_bound, Var::Start(j, i));
-
-            let left_on_job: i32 = (i..pb.num_machines).map(|t| pb.duration(j, t)).sum();
-            m.enforce(leq(task_start + left_on_job, makespan_variable));
-
-            if i > 0 {
-                m.enforce(leq(end(&m, j, i - 1), task_start));
+        for m1 in 0..pb.num_machines {
+            let task_start = m.new_ivar(0, upper_bound, Var::Start(j, m1));
+            m.enforce(leq(task_start + pb.duration(j, m1), makespan_variable));
+        }
+    }
+    for j in 0..pb.num_jobs {
+        for m1 in 0..pb.num_machines {
+            for m2 in (m1 + 1)..pb.num_machines {
+                let prec = m.new_bvar(Var::Prec(j, m1, j, m2));
+                m.bind(leq(end(&m, j, m1), start(&m, j, m2)), prec.true_lit());
+                m.bind(leq(end(&m, j, m2), start(&m, j, m1)), prec.false_lit());
             }
         }
     }
-    for machine in 1..(pb.num_machines + 1) {
+    for machine in 0..(pb.num_machines) {
         for j1 in 0..pb.num_jobs {
             for j2 in (j1 + 1)..pb.num_jobs {
-                let i1 = pb.op_with_machine(j1, machine);
-                let i2 = pb.op_with_machine(j2, machine);
-
                 // variable that is true if (j1, i1) comes first and false otherwise.
                 // in any case, setting a value to it enforces that the two tasks do not overlap
-                let prec = m.new_bvar(Var::Prec(j1, i1, j2, i2));
-                m.bind(leq(end(&m, j1, i1), start(&m, j2, i2)), prec.true_lit());
-                m.bind(leq(end(&m, j2, i2), start(&m, j1, i1)), prec.false_lit());
+                let prec = m.new_bvar(Var::Prec(j1, machine, j2, machine));
+                m.bind(leq(end(&m, j1, machine), start(&m, j2, machine)), prec.true_lit());
+                m.bind(leq(end(&m, j2, machine), start(&m, j1, machine)), prec.false_lit());
             }
         }
     }
     m
 }
 
-struct ResourceOrderingFirst;
-impl Heuristic<Var> for ResourceOrderingFirst {
-    fn decision_stage(&self, _var: VarRef, label: Option<&Var>, _model: &aries_model::Model<Var>) -> u8 {
-        match label {
-            Some(&Var::Prec(_, _, _, _)) => 0, // a reification of (a <= b), decide in the first stage
-            Some(&Var::Makespan) | Some(&Var::Start(_, _)) => 1, // delay decisions on the temporal variable to the second stage
-            _ => 2,
-        }
-    }
-}
-
 /// Builds a solver for the given strategy.
-fn get_solver(base: Solver, strategy: SearchStrategy, est_brancher: EstBrancher) -> ParSolver {
+pub fn get_solver(base: Solver, strategy: SearchStrategy, est_brancher: EstBrancher) -> ParSolver {
     let base_solver = Box::new(base);
     let make_act = |s: &mut Solver| s.set_brancher(ActivityBrancher::new_with_heuristic(ResourceOrderingFirst));
     let make_est = |s: &mut Solver| s.set_brancher(est_brancher.clone());
@@ -300,8 +261,8 @@ fn get_solver(base: Solver, strategy: SearchStrategy, est_brancher: EstBrancher)
 }
 
 #[derive(Clone)]
-struct EstBrancher {
-    pb: JobShop,
+pub struct EstBrancher {
+    pb: OpenShop,
     saved: DecLvl,
 }
 
