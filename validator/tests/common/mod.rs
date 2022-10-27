bb3f13d271a88fe04722f5e351183e40650841fb
Roland Godet <r.godet@outlook.fr>
2022-10-27 15:47:39 +0200

    test(validator): implement valid tests

---
+++ validator/tests/common/mod.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+use prost::Message;
+use std::fs::read;
+use std::path::PathBuf;
+
+use unified_planning::{Plan, Problem};
+
+fn get_bin(name: &str, folder: &str) -> Result<Vec<u8>> {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("tests/bin");
+    path.push(folder);
+    path.push(name);
+    path.set_extension("bin");
+    let bin = read(path)?;
+    Ok(bin)
+}
+
+pub fn get_problem(name: &str) -> Result<Problem> {
+    let bin = get_bin(name, "problems")?;
+    let problem = Problem::decode(bin.as_slice())?;
+    Ok(problem)
+}
+
+pub fn get_plan(name: &str) -> Result<Plan> {
+    let bin = get_bin(name, "plans")?;
+    let plan = Plan::decode(bin.as_slice())?;
+    Ok(plan)
+}
---
+++ validator/tests/integration_test.rs
@@ -0,0 +1,65 @@
+use anyhow::Result;
+use aries_plan_validator::validate;
+
+mod common;
+
+fn valid_plan(name: &str) -> Result<()> {
+    let problem = common::get_problem(name)?;
+    let plan = common::get_plan(name)?;
+    validate(&problem, &plan)
+}
+
+#[cfg(test)]
+mod test {
+    use super::valid_plan;
+
+    macro_rules! make_test {
+        ($name:expr) => {
+            paste::item! {
+                #[test]
+                fn [< test_ $name >] () {
+                    let result = valid_plan($name);
+                    assert!(result.is_ok(), "\x1b[91m{:?}\x1b[0m", result.err().unwrap());
+                }
+            }
+        };
+    }
+
+    make_test!("matchcellar");
+    make_test!("basic_conditional");
+    make_test!("counter_to_50");
+    make_test!("robot_loader_mod");
+    make_test!("hierarchical_blocks_world_exists");
+    make_test!("counter");
+    make_test!("travel");
+    make_test!("robot_no_negative_preconditions");
+    make_test!("hierarchical_blocks_world");
+    make_test!("basic_without_negative_preconditions");
+    make_test!("basic_forall");
+    make_test!("basic_oversubscription");
+    make_test!("robot_with_static_fluents_duration");
+    make_test!("travel_with_consumptions");
+    make_test!("robot_fluent_of_user_type");
+    make_test!("hierarchical_blocks_world_with_object");
+    make_test!("timed_connected_locations");
+    make_test!("robot_loader");
+    make_test!("basic_with_object_constant");
+    make_test!("basic_nested_conjunctions");
+    make_test!("complex_conditional");
+    make_test!("htn-go");
+    make_test!("robot_real_constants");
+    make_test!("basic_exists");
+    make_test!("robot_decrease");
+    make_test!("basic_with_costs");
+    make_test!("robot_locations_connected_without_battery");
+    make_test!("robot_loader_adv");
+    make_test!("basic");
+    make_test!("robot_locations_connected");
+    make_test!("hierarchical_blocks_world_object_as_root");
+    make_test!("robot_int_battery");
+    make_test!("charge_discharge");
+    make_test!("robot_locations_visited");
+    make_test!("temporal_conditional");
+    make_test!("robot");
+    make_test!("robot_fluent_of_user_type_with_int_id");
+}
