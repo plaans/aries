e0e5ecf5060de835e4393e57f654f48c03f3709f
Arthur Bit-Monnot <abitmonnot@laas.fr>
2022-12-17 22:41:11 +0100

    [sched] Remove unuecessary layers in conflicts-based heuristic.

--- examples/scheduling/src/search/conflicts.rs
+++ examples/scheduling/src/search/conflicts.rs
@@ -10,24 +10,8 @@ use aries_model::Model;
 use aries_solver::solver::search::{Decision, SearchControl};
 use aries_solver::solver::stats::Stats;
 use itertools::Itertools;
-use std::cmp::Ordering;
 use std::collections::HashSet;
-use std::fmt::{Debug, Formatter};
-
-#[derive(PartialEq)]
-#[allow(unused)]
-enum Mode {
-    Var,
-    Sum,
-    Max,
-    Min,
-}
-
-#[derive(PartialEq, Copy, Clone, Debug)]
-pub enum Heuristic {
-    Vsids,
-    LearningRate,
-}
+use std::fmt::Debug;
 
 #[derive(Default, Clone)]
 struct ConflictTracking {
@@ -41,7 +25,7 @@ struct ConflictTracking {
 #[derive(Clone)]
 pub struct ConflictBasedBrancher {
     heap: VarSelect,
-    default_assignment: DefaultValues,
+    default_assignment: PreferredValues,
     num_processed_var: usize,
     /// Associates presence literals to the optional variables
     /// Essentially a Map<Lit, Set<VarRef>>
@@ -51,14 +35,31 @@ pub struct ConflictBasedBrancher {
     conflicts: ConflictTracking,
 }
 
+/// Optionally associates to each variable a value that is its preferred one.
 #[derive(Clone, Default)]
-struct DefaultValues {
+struct PreferredValues {
     /// If these default values came from a valid assignment, this is the value of the associated objective
     objective_found: Option<IntCst>,
     /// Default value for variables (some variables might not have one)
     values: RefMap<VarRef, IntCst>,
 }
 
+#[derive(Copy, Clone)]
+pub struct Params {
+    /// Which scheme to use to define the priority of a variable, based on the conflict it participates in.
+    pub heuristic: Heuristic,
+    /// How do we determine that a variable participates in a conflict.
+    pub active: ActiveLiterals,
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub enum Heuristic {
+    /// Priority is the activity of the variable: its activity is bumped each time it participates in a conflict.
+    Vsids,
+    /// Priority is the learning rate of the variable: ration of conflict in which it participates when set
+    /// Ref: Learning Rate Based Branching Heuristic for SAT Solvers
+    LearningRate,
+}
 /// Controls which literals are considered active in a conflict.
 /// Ref: Learning Rate Based Branching Heuristic for SAT Solvers
 #[derive(PartialOrd, PartialEq, Eq, Copy, Clone, Debug)]
@@ -70,12 +71,6 @@ pub enum ActiveLiterals {
     /// Literals of the clause + literals resolved when producing the clause + literals that
     /// appear in the explanation of the literals in teh clause.
     Reasoned,
-}
-
-#[derive(Copy, Clone)]
-pub struct Params {
-    pub active: ActiveLiterals,
-    pub heuristic: Heuristic,
 }
 
 impl Default for Params {
@@ -96,7 +91,7 @@ impl ConflictBasedBrancher {
         ConflictBasedBrancher {
             params,
             heap: VarSelect::new(Default::default()),
-            default_assignment: DefaultValues::default(),
+            default_assignment: PreferredValues::default(),
             num_processed_var: 0,
             presences: Default::default(),
             cursor: ObsTrailCursor::new(),
@@ -142,6 +137,7 @@ impl ConflictBasedBrancher {
             }
             let v = x.affected_bound.variable();
             if self.is_decision_variable(x.affected_bound.variable()) {
+                // TODO: this assume the variable is binary (and thus set on the first event)
                 self.conflicts.conflict_since_assignment.fill_with(v, || 0);
                 assert!(!self.conflicts.assignment_time.contains(v));
                 self.conflicts.assignment_time.insert(v, self.conflicts.num_conflicts);
@@ -241,39 +237,7 @@ impl Default for BoolHeuristicParams {
     }
 }
 
-const MODE: Mode = Mode::Var;
-
-/// Heuristic value associated to a variable.
-#[derive(Copy, Clone, PartialEq)]
-struct BoolVarHeuristicValue {
-    activity_one: f32,
-    activity_zero: f32,
-}
-impl BoolVarHeuristicValue {
-    fn summary(&self) -> f32 {
-        match MODE {
-            Mode::Var => {
-                debug_assert!(self.activity_zero == 0.0);
-                self.activity_one
-            }
-            Mode::Sum => self.activity_zero + self.activity_one,
-            Mode::Max => self.activity_zero.max(self.activity_one),
-            Mode::Min => self.activity_zero.min(self.activity_one),
-        }
-    }
-}
-impl PartialOrd for BoolVarHeuristicValue {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.summary().partial_cmp(&other.summary())
-    }
-}
-impl Debug for BoolVarHeuristicValue {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} / {}", self.activity_one, self.activity_zero)
-    }
-}
-
-type Heap = IdxHeap<VarRef, BoolVarHeuristicValue>;
+type Heap = IdxHeap<VarRef, f32>;
 
 /// Changes that need to be undone.
 /// The only change that we need to undo is the removal from the queue.
@@ -288,10 +252,11 @@ enum HeapEvent {
 #[derive(Clone)]
 struct VarSelect {
     params: BoolHeuristicParams,
-    /// One heap for each decision stage.
+    /// Heap where pending variables are present and allows extracting the highest-priority variable.
     heap: Heap,
-    /// Stage in which each variable appears.
-    stages: HashSet<VarRef>,
+    /// Decision variables among which we must select.
+    vars: HashSet<VarRef>,
+    /// Trail that record events and allows repopulating the heap when backtracking.
     trail: Trail<HeapEvent>,
 }
 
@@ -300,27 +265,24 @@ impl VarSelect {
         VarSelect {
             params,
             heap: Default::default(),
-            stages: Default::default(),
+            vars: Default::default(),
             trail: Trail::default(),
         }
     }
 
     pub fn is_declared(&self, v: VarRef) -> bool {
-        self.stages.contains(&v)
+        self.vars.contains(&v)
     }
 
     /// Declares a new variable. The variable is NOT added to the queue.
     /// The stage parameter defines at which stage of the search the variable will be selected.
     /// Variables with the lowest stage are considered first.
-    pub fn declare_variable(&mut self, v: VarRef, initial_activity: Option<f32>) {
+    pub fn declare_variable(&mut self, v: VarRef, initial_priority: Option<f32>) {
         debug_assert!(!self.is_declared(v));
-        let hvalue = BoolVarHeuristicValue {
-            activity_one: initial_activity.unwrap_or(0.0),
-            activity_zero: initial_activity.unwrap_or(0.0),
-        };
+        let priority = initial_priority.unwrap_or(0.0);
 
-        self.heap.declare_element(v, hvalue);
-        self.stages.insert(v);
+        self.heap.declare_element(v, priority);
+        self.vars.insert(v);
     }
 
     /// Adds a previously declared variable to its queue
@@ -352,19 +314,12 @@ impl VarSelect {
 
     fn lit_increase_activity(&mut self, lit: Lit, factor: f32) {
         let var = lit.variable();
-        let is_one = lit == var.geq(1);
-        if self.stages.contains(&var) {
+        if self.vars.contains(&var) {
             let var_inc = self.params.var_inc * factor;
 
-            self.heap.change_priority(var, |p| {
-                if is_one || MODE == Mode::Var {
-                    p.activity_one += var_inc
-                } else {
-                    p.activity_zero += var_inc
-                }
-            });
+            self.heap.change_priority(var, |p| *p += var_inc);
             let p = self.heap.priority(var);
-            if p.activity_one > 1e30_f32 || p.activity_zero > 1e30_f32 {
+            if p > 1e30_f32 {
                 self.var_rescale_activity()
             }
         }
@@ -374,8 +329,7 @@ impl VarSelect {
         debug_assert!(!new_value.is_nan());
         debug_assert!(!factor.is_nan());
         let var = lit.variable();
-        let is_one = lit == var.geq(1) || MODE == Mode::Var;
-        if self.stages.contains(&var) {
+        if self.vars.contains(&var) {
             // assert!(self.params.var_inc == 1.0_f32);
 
             let factor = factor as f64;
@@ -383,8 +337,7 @@ impl VarSelect {
 
             let new_priority = loop {
                 let var_inc = self.params.var_inc as f64;
-                let p = self.heap.priority(var);
-                let previous = if is_one { p.activity_one } else { p.activity_zero } as f64;
+                let previous = self.heap.priority(var) as f64;
                 // the value was decayed N times, we undo this by multiplying it by (decay_factor^(-N))
                 // this can result in very large numbers, hence the usage of f64 to avoid over shouting
                 // to avoid rare case, (1/0.95)^14000 = infinity, we saturate very high
@@ -392,11 +345,7 @@ impl VarSelect {
                     .powi(-(num_decays_to_undo as i32))
                     .min(1e300_f64);
                 let corrected = previous * correction;
-                // assert!(
-                //     corrected / var_inc <= 1.0,
-                //     "{previous}  {correction}  {corrected}  {var_inc}  {}",
-                //     corrected / var_inc
-                // );
+                // we might loose a lot of precision in the above multiplication, make sure we stay within the normal bounds
                 let corrected = corrected.clamp(0.0, var_inc);
                 let new = corrected * (1.0 - factor) + new_value * factor * var_inc;
                 if new > 1e30_f64 {
@@ -404,20 +353,16 @@ impl VarSelect {
                     // I suspect that in extreme cases, several rescale might be necessary, hence the loop
                     self.var_rescale_activity();
                 } else {
+                    // we would fit in an f32, proceed with the update
                     break new;
                 }
             } as f32;
-            // println!("{}", new_priority / self.params.var_inc);
-            let p = new_priority / self.params.var_inc;
-            debug_assert!(p <= 1.1, "{p}");
-            debug_assert!(p >= -0.1, "{p}");
-            self.heap.change_priority(var, |p| {
-                if is_one {
-                    p.activity_one = new_priority;
-                } else {
-                    p.activity_zero = new_priority
-                };
+
+            debug_assert!({
+                let p = new_priority / self.params.var_inc;
+                -0.1 <= p && p <= 1.1
             });
+            self.heap.change_priority(var, |p| *p = new_priority);
         }
     }
 
@@ -433,8 +378,7 @@ impl VarSelect {
         // here we scale the activity of all variables, to avoid overflowing
         // this can not change the relative order in the heap, since activities are scaled by the same amount.
         self.heap.change_all_priorities_in_place(|p| {
-            p.activity_one *= 1e-30_f32;
-            p.activity_zero *= 1e-30_f32;
+            *p *= 1e-30_f32;
         });
         self.params.var_inc *= 1e-30_f32;
     }
