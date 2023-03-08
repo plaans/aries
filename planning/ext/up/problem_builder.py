ccd170522e75b836111a208c6542380b485e83fb
Roland Godet <r.godet@outlook.fr>
2023-03-08 10:07:49 +0100

    fix(up): binaries creation

--- planning/ext/up/problem_builder.py
+++ planning/ext/up/problem_builder.py
@@ -1,6 +1,8 @@
-import os
+"""Module to export the problems and the plans of the unified_planning module in binary."""
+
 from pathlib import Path
 import sys
+from typing import IO
 
 # Use the local version of the UP in the `planning/ext/up/unified_planning` git submodule
 upf_path = Path(__file__).resolve().parent / "unified_planning"
@@ -15,44 +17,54 @@ except ImportError as err:
     ) from err
 
 
-class BuildProblems:
-    OUT_DIR = Path(__file__).resolve().parent / "bins"
-    WRITER = ProtobufWriter()
-    examples = list(get_example_problems().keys())
+class Factory:
+    """Factory for the problems and the plans of the unified_planning module in binary."""
+
+    _OUT_DIR = Path(__file__).resolve().parent / "bins"
+    _writer = ProtobufWriter()
+    _examples = get_example_problems()
 
     def __init__(self) -> None:
-        self.OUT_DIR.mkdir(parents=True, exist_ok=True)
-        (self.OUT_DIR / "problems").mkdir(parents=True, exist_ok=True)
-        (self.OUT_DIR / "plans").mkdir(parents=True, exist_ok=True)
+        self._OUT_DIR.mkdir(parents=True, exist_ok=True)
+        (self._OUT_DIR / "problems").mkdir(parents=True, exist_ok=True)
+        (self._OUT_DIR / "plans").mkdir(parents=True, exist_ok=True)
 
-    def export_problems(self):
-        for p in self.examples:
-            with open(os.path.join(self.OUT_DIR, "problems/" + p + ".bin"), "wb") as f:
-                print(f"Exporting problem {p}")
-                data = get_example_problems()[p].problem
-                try:
-                    data = self.WRITER.convert(data)
-                except Exception as _e:
-                    continue
-                f.write(data.SerializeToString())
+    @classmethod
+    def problem_file(cls, name: str) -> Path:
+        """Returns the path of the problem binary with the given name."""
+        return cls._OUT_DIR / "problems" / f"{name}.bin"
 
-    def export_plans(self):
-        for p in self.examples:
-            with open(os.path.join(self.OUT_DIR, "plans/" + p + ".bin"), "wb") as f:
-                print(f"Exporting plan {p}")
-                data = get_example_problems()[p].plan
-                try:
-                    data = self.WRITER.convert(data)
-                except Exception as _e:
-                    continue
-                f.write(data.SerializeToString())
+    @classmethod
+    def plan_file(cls, name: str) -> Path:
+        """Returns the path of the plan binary with the given name."""
+        return cls._OUT_DIR / "plans" / f"{name}.bin"
 
+    @classmethod
+    def _export(cls, data, file: IO):
+        """Exports the data in the file in binary."""
+        try:
+            data = cls._writer.convert(data)
+        except Exception:  # nosec: B112  # pylint: disable = W0718
+            return
+        file.write(data.SerializeToString())
 
-def main():
-    builder = BuildProblems()
-    builder.export_problems()
-    builder.export_plans()
+    @classmethod
+    def export_problems(cls):
+        """Exports all the problems."""
+        for name, (problem, _) in cls._examples.items():
+            with open(cls.problem_file(name), "wb") as file:
+                print(f"Exporting problem {name}")
+                cls._export(problem, file)
+
+    @classmethod
+    def export_plans(cls):
+        """Exports all the plans."""
+        for name, (_, plan) in cls._examples.items():
+            with open(cls.plan_file(name), "wb") as file:
+                print(f"Exporting plan {name}")
+                cls._export(plan, file)
 
 
 if __name__ == "__main__":
-    main()
+    Factory.export_problems()
+    Factory.export_plans()
