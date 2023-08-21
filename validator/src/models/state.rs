d75897fddcd8febf3fe356055752b619cc40dd54
Roland Godet <r.godet@outlook.fr>
2023-08-21 18:36:27 +0200

    chore(val): check fluent bounds when setting the value

--- validator/src/models/state.rs
+++ validator/src/models/state.rs
@@ -1,4 +1,6 @@
-use anyhow::Result;
+use crate::utils::extract_bounds;
+use anyhow::{bail, ensure, Context, Result};
+use malachite::Rational;
 use std::fmt::Display;
 
 use im::HashMap;
@@ -15,9 +17,27 @@ pub struct State(HashMap<Vec<Value>, Value>);
 
 impl State {
     /// Bounds a fluent to a value.
+    ///
+    /// Returns an error if the value is out of the bounds of the fluent
     pub fn bound(&mut self, k: Vec<Value>, v: Value) -> Result<Option<Value>> {
-        // TODO - Check bounds
-        Ok(self.0.insert(k, v))
+        let f = k.first().context("Fluent with an empty signature")?;
+        match f {
+            Value::Symbol(s) => {
+                let opt_bounds = extract_bounds(s)?;
+                if let Some((lb, ub)) = opt_bounds {
+                    match v.clone() {
+                        Value::Number(v, _, _) => {
+                            let r_lb: Rational = lb.into();
+                            let r_ub: Rational = ub.into();
+                            ensure!(r_lb <= v && v <= r_ub);
+                        }
+                        _ => bail!("Try to set a not number value into a fluent with bounds"),
+                    }
+                };
+                Ok(self.0.insert(k, v))
+            }
+            _ => bail!("Fluent without a symbol as name"),
+        }
     }
 
     /// Returns a reference to the value corresponding to the fluent.
@@ -68,6 +88,18 @@ mod tests {
         let mut s = State::default();
         s.bound(k("s"), v(true))?;
         assert_eq!(s.0, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn bound_with_bounds() -> Result<()> {
+        let f = k("s - integer[0, 10]");
+        let mut s = State::default();
+
+        assert!(s.bound(f.clone(), 15.into()).is_err());
+        assert!(s.0.is_empty());
+        s.bound(f.clone(), 5.into())?;
+        assert_eq!(s.0, hashmap! {f => 5.into()});
         Ok(())
     }
 
