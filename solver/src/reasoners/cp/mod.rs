8210af64ec470b4c9cd7c27ae9a9ae78a781d474
Arthur Bit-Monnot <abitmonnot@laas.fr>
2023-11-22 17:43:27 +0100

    chore(cp): Clean up internal conversions from/to i64.

--- solver/src/reasoners/cp/mod.rs
+++ solver/src/reasoners/cp/mod.rs
@@ -68,17 +68,16 @@ impl std::fmt::Display for LinearSumLeq {
 }
 
 impl LinearSumLeq {
-    fn get_lower_bound(&self, elem: SumElem, domains: &Domains) -> IntCst {
+    fn get_lower_bound(&self, elem: SumElem, domains: &Domains) -> i64 {
         let var_is_present = domains.present(elem.var) == Some(true);
         debug_assert!(!domains.entails(elem.lit) || var_is_present);
 
         let int_part = match elem.factor.cmp(&0) {
-            Ordering::Less => domains.ub(elem.var),
+            Ordering::Less => domains.ub(elem.var) as i64,
             Ordering::Equal => 0,
-            Ordering::Greater => domains.lb(elem.var),
+            Ordering::Greater => domains.lb(elem.var) as i64,
         }
-        .saturating_mul(elem.factor)
-        .clamp(INT_CST_MIN, INT_CST_MAX);
+        .saturating_mul(elem.factor as i64);
 
         match domains.value(elem.lit) {
             Some(true) => int_part,
@@ -86,17 +85,16 @@ impl LinearSumLeq {
             None => 0.min(int_part),
         }
     }
-    fn get_upper_bound(&self, elem: SumElem, domains: &Domains) -> IntCst {
+    fn get_upper_bound(&self, elem: SumElem, domains: &Domains) -> i64 {
         let var_is_present = domains.present(elem.var) == Some(true);
         debug_assert!(!domains.entails(elem.lit) || var_is_present);
 
         let int_part = match elem.factor.cmp(&0) {
-            Ordering::Less => domains.lb(elem.var),
+            Ordering::Less => domains.lb(elem.var) as i64,
             Ordering::Equal => 0,
-            Ordering::Greater => domains.ub(elem.var),
+            Ordering::Greater => domains.ub(elem.var) as i64,
         }
-        .saturating_mul(elem.factor)
-        .clamp(INT_CST_MIN, INT_CST_MAX);
+        .saturating_mul(elem.factor as i64);
 
         match domains.value(elem.lit) {
             Some(true) => int_part,
@@ -104,7 +102,9 @@ impl LinearSumLeq {
             None => 0.max(int_part),
         }
     }
-    fn set_ub(&self, elem: SumElem, ub: IntCst, domains: &mut Domains, cause: Cause) -> Result<bool, InvalidUpdate> {
+    fn set_ub(&self, elem: SumElem, ub: i64, domains: &mut Domains, cause: Cause) -> Result<bool, InvalidUpdate> {
+        // convert back to i32 (which is used for domains). Clamping should prevent any conversion error.
+        let ub = ub.clamp(INT_CST_MIN as i64, INT_CST_MAX as i64) as i32;
         debug_assert!(!domains.entails(elem.lit) || domains.present(elem.var) == Some(true));
         // println!(
         //     "  {:?} : [{}, {}]    ub: {ub}   -> {}",
@@ -156,7 +156,7 @@ impl Propagator for LinearSumLeq {
                 .iter()
                 .copied()
                 .filter(|e| !domains.entails(!e.lit))
-                .map(|e| self.get_lower_bound(e, domains) as i64)
+                .map(|e| self.get_lower_bound(e, domains))
                 .sum();
             let f = (self.ub as i64) - sum_lb;
             // println!("Propagation : {} <= {}", sum_lb, self.ub);
@@ -168,13 +168,12 @@ impl Propagator for LinearSumLeq {
                 return Err(Contradiction::Explanation(expl));
             }
             for &e in &self.elements {
-                let lb = self.get_lower_bound(e, domains) as i64;
-                let ub = self.get_upper_bound(e, domains) as i64;
+                let lb = self.get_lower_bound(e, domains);
+                let ub = self.get_upper_bound(e, domains);
                 debug_assert!(lb <= ub);
                 if ub - lb > f {
                     // println!("  problem on: {e:?} {lb} {ub}");
-                    // NOTE: Conversion from i64 to i32 should not fail due to the clamp between two i32 values.
-                    let new_ub = (f + lb).clamp(INT_CST_MIN as i64, INT_CST_MAX as i64) as i32;
+                    let new_ub = (f + lb);
                     match self.set_ub(e, new_ub, domains, cause) {
                         Ok(true) => {} // println!("    propagated: {e:?} <= {}", f + lb),
                         Ok(false) => {}
