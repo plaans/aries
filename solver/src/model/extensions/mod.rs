546a4a5a62722c1ae3422b5f217611e25997480a
Arthur Bit-Monnot <abitmonnot@laas.fr>
2024-06-14 22:43:47 +0200

    chore: renaming to avoid name clash of PartialAssignment

--- solver/src/model/extensions/mod.rs
+++ solver/src/model/extensions/mod.rs
@@ -18,7 +18,7 @@ use crate::core::state::{Domains, IntDomain};
 use crate::core::*;
 use crate::model::lang::IAtom;
 
-pub trait PartialAssignment {
+pub trait PartialBoolAssignment {
     fn entails(&self, literal: Lit) -> bool;
     fn value(&self, literal: Lit) -> Option<bool> {
         if self.entails(literal) {
@@ -31,7 +31,7 @@ pub trait PartialAssignment {
     }
 }
 
-impl PartialAssignment for Domains {
+impl PartialBoolAssignment for Domains {
     fn entails(&self, literal: Lit) -> bool {
         self.entails(literal)
     }
