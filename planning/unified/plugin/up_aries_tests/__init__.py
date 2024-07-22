43ee23f7d81ceee10962974a35742d53a6837434
Roland Godet <r.godet@outlook.fr>
2024-07-22 14:12:38 +0200

    tests: add for action cost based on action parameter

--- planning/unified/plugin/up_aries_tests/__init__.py
+++ planning/unified/plugin/up_aries_tests/__init__.py
@@ -6,10 +6,13 @@ import unified_planning.model.scheduling as sched
 def get_test_cases():
     """Creates on test case for each function starting with `test_` in the file"""
     res = {}
-    gens = [(name.removeprefix('test_'), fn) for name, fn in globals().items()
-            if callable(fn) and fn.__module__ == __name__ and name.startswith('test_')]
+    gens = [
+        (name.removeprefix("test_"), fn)
+        for name, fn in globals().items()
+        if callable(fn) and fn.__module__ == __name__ and name.startswith("test_")
+    ]
 
-    for (name, generator) in gens:
+    for name, generator in gens:
         print(name)
         res[name] = generator()
 
@@ -19,9 +22,9 @@ def get_test_cases():
 def test_sched_bool_param():
     problem = sched.SchedulingProblem()
 
-    a = problem.add_activity('a', 10)
+    a = problem.add_activity("a", 10)
 
-    a.add_parameter('p', BoolType())
+    a.add_parameter("p", BoolType())
 
     return TestCase(problem=problem, solvable=True)
 
@@ -30,4 +33,22 @@ def test_half_bounded_int_param():
     problem = Problem()
     problem.add_action(InstantaneousAction("a", k=IntType(lower_bound=0)))
     problem.add_action(InstantaneousAction("b", k=IntType(upper_bound=10)))
+    return TestCase(problem=problem, solvable=True)
+
+
+def test_action_costs():
+    problem = Problem()
+    pa = problem.add_fluent("pa", BoolType(), default_initial_value=False)
+    pb = problem.add_fluent("pb", BoolType(), default_initial_value=False)
+    a = InstantaneousAction("a")
+    a.add_precondition(Not(pa))
+    a.add_effect(pa, True)
+    b = InstantaneousAction("b", k=IntType())
+    b.add_precondition(Not(pb))
+    b.add_effect(pb, True)
+    costs = {a: 10, b: b.k}
+    problem.add_actions([a, b])
+    problem.add_quality_metric(MinimizeActionCosts(costs, 1))
+    problem.add_goal(pa)
+    problem.add_goal(pb)
     return TestCase(problem=problem, solvable=True)
