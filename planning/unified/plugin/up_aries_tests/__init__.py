a6db8c1c3b1272af17359aa5cae0b799cfa556e5
Roland Godet <r.godet@outlook.fr>
2024-07-22 11:44:04 +0200

    tests: add for action cost based on action parameter

--- planning/unified/plugin/up_aries_tests/__init__.py
+++ planning/unified/plugin/up_aries_tests/__init__.py
@@ -24,3 +24,21 @@ def test_sched_bool_param():
     a.add_parameter('p', BoolType())
 
     return TestCase(problem=problem, solvable=True)
+
+
+def test_action_costs():
+    problem = Problem()
+    pa = problem.add_fluent("pa", BoolType(), default_initial_value=False)
+    pb = problem.add_fluent("pb", BoolType(), default_initial_value=False)
+    a = InstantaneousAction("a")
+    a.add_precondition(Not(pa))
+    a.add_effect(pa, True)
+    b = InstantaneousAction("b", k=IntType())
+    b.add_precondition(Not(pb))
+    b.add_effect(pb, True)
+    costs = {a: 10, b: b.k}
+    problem.add_actions([a, b])
+    problem.add_quality_metric(MinimizeActionCosts(costs, 1))
+    problem.add_goal(pa)
+    problem.add_goal(pb)
+    return TestCase(problem=problem, solvable=True)
