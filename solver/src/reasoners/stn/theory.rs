e12ff390fffc5a3eb320c5b0cc344db304909e44
Arthur Bit-Monnot <abitmonnot@laas.fr>
2024-11-20 20:42:27 +0100

    deactivate bounds propagation by default

--- solver/src/reasoners/stn/theory.rs
+++ solver/src/reasoners/stn/theory.rs
@@ -28,7 +28,7 @@ pub type Timepoint = VarRef;
 pub type W = IntCst;
 
 pub static STN_THEORY_PROPAGATION: EnvParam<TheoryPropagationLevel> =
-    EnvParam::new("ARIES_STN_THEORY_PROPAGATION", "full");
+    EnvParam::new("ARIES_STN_THEORY_PROPAGATION", "bounds");
 pub static STN_EXTENSIVE_TESTS: EnvParam<bool> = EnvParam::new("ARIES_STN_EXTENSIVE_TESTS", "false");
 
 /// Describes which part of theory propagation should be enabled.
