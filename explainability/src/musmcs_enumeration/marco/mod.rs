6ffa825f4100a581a9d3a742bb42b92202c75ec5
Nika Beriachvili <nikaberiachvilipro@gmail.com>
2024-12-26 16:09:15 +0100

    fix(expl): fix map solver, refactor in detail, improve usage of iterators and clones

--- explainability/src/musmcs_enumeration/marco/mod.rs
+++ explainability/src/musmcs_enumeration/marco/mod.rs
@@ -1,15 +1,21 @@
-mod simple_marco;
+// mod filtered_marco;
+pub mod simple_marco;
 
 use super::{MusMcsEnumerationConfig, MusMcsEnumerationResult};
 
 use aries::core::Lit;
 use aries::model::{Label, Model};
 use aries::reif::Reifiable;
-use aries::solver::Solver;
 use std::collections::BTreeSet;
 
-trait Marco<Lbl: Label> {
-    fn new<Expr: Reifiable<Lbl> + Copy>(
+pub trait Marco<Lbl: Label> {
+    fn new_with_soft_constrs_reif_lits(
+        model: Model<Lbl>,
+        soft_constrs_reif_lits: Vec<Lit>,
+        config: MusMcsEnumerationConfig,
+    ) -> Self;
+
+    fn new_with_soft_constrs<Expr: Reifiable<Lbl>>(
         model: Model<Lbl>,
         soft_constrs: Vec<Expr>,
         config: MusMcsEnumerationConfig,
@@ -17,9 +23,9 @@ trait Marco<Lbl: Label> {
 
     fn reset_result(&mut self);
 
-    fn clone_result(&self) -> MusMcsEnumerationResult<Lbl>;
+    fn clone_result(&self) -> MusMcsEnumerationResult;
 
-    fn get_result(&self) -> &MusMcsEnumerationResult<Lbl>;
+    fn get_expr_reif_lit<Expr: Reifiable<Lbl>>(&mut self, soft_constr: Expr) -> Result<Lit, ()>;
 
     fn find_unexplored_seed(&mut self) -> bool;
 
@@ -29,7 +35,7 @@ trait Marco<Lbl: Label> {
 
     fn do_case_seed_unsat(&mut self);
 
-    fn run(&mut self) -> MusMcsEnumerationResult<Lbl> {
+    fn run(&mut self) -> MusMcsEnumerationResult {
         self.reset_result();
         while self.find_unexplored_seed() {
             if self.check_seed_sat() {
@@ -42,14 +48,14 @@ trait Marco<Lbl: Label> {
     }
 }
 
-trait MapSolver<Lbl: Label> {
+trait MapSolver {
     fn find_unexplored_seed(&mut self) -> Option<BTreeSet<Lit>>;
 
     fn block_down(&mut self, frompoint: &BTreeSet<Lit>);
 
     fn block_up(&mut self, frompoint: &BTreeSet<Lit>);
 
-    fn get_internal_solver(&mut self) -> &mut Solver<Lbl>;
+    // fn get_internal_solver(&mut self) -> &mut Solver<u8>;
 }
 
 trait SubsetSolver<Lbl: Label> {
@@ -59,7 +65,7 @@ trait SubsetSolver<Lbl: Label> {
 
     fn shrink(&mut self, seed: &BTreeSet<Lit>) -> BTreeSet<Lit>;
 
-    fn get_internal_solver(&mut self) -> &mut Solver<Lbl>;
+    // fn get_internal_solver(&mut self) -> &mut Solver<Lbl>;
 }
 
 #[cfg(test)]
@@ -68,6 +74,7 @@ mod tests {
     use std::collections::BTreeSet;
 
     use aries::model::lang::expr::lt;
+    use itertools::Itertools;
 
     use super::{Marco, MusMcsEnumerationConfig};
 
@@ -80,46 +87,49 @@ mod tests {
         let x0 = model.new_ivar(0, 10, "x0");
         let x1 = model.new_ivar(0, 10, "x1");
         let x2 = model.new_ivar(0, 10, "x2");
-        let soft_constrs = vec![lt(x0, x1), lt(x1, x2), lt(x2, x0), lt(x0, x2)];
 
-        let mut simple_marco = SimpleMarco::new(
+        let soft_constrs = vec![lt(x0, x1), lt(x1, x2), lt(x2, x0), lt(x0, x2)];
+        let mut simple_marco = SimpleMarco::new_with_soft_constrs(
             model,
-            soft_constrs,
+            soft_constrs.clone(),
             MusMcsEnumerationConfig {
                 return_muses: true,
                 return_mcses: true,
             },
         );
-        let res = simple_marco.run();
+        let soft_constrs_reif_lits = soft_constrs
+            .into_iter()
+            .map(|expr| simple_marco.get_expr_reif_lit(expr))
+            .collect_vec();
 
-        let computed_muses = res.muses_reif_lits.unwrap().into_iter().collect::<BTreeSet<_>>();
-        let computed_mcses = res.mcses_reif_lits.unwrap().into_iter().collect::<BTreeSet<_>>();
+        let res = simple_marco.run();
+        let res_muses = res.muses.unwrap().into_iter().collect::<BTreeSet<_>>();
+        let res_mcses = res.mcses.unwrap().into_iter().collect::<BTreeSet<_>>();
 
         let expected_muses = BTreeSet::from_iter(vec![
             BTreeSet::from_iter(vec![
-                res.soft_constrs_reifs.get_reif_lit(0, 0),
-                res.soft_constrs_reifs.get_reif_lit(1, 0),
-                res.soft_constrs_reifs.get_reif_lit(2, 0),
+                soft_constrs_reif_lits[0].unwrap(),
+                soft_constrs_reif_lits[1].unwrap(),
+                soft_constrs_reif_lits[2].unwrap(),
             ]),
             BTreeSet::from_iter(vec![
-                res.soft_constrs_reifs.get_reif_lit(2, 0),
-                res.soft_constrs_reifs.get_reif_lit(3, 0),
+                soft_constrs_reif_lits[2].unwrap(),
+                soft_constrs_reif_lits[3].unwrap(),
             ]),
         ]);
-
         let expected_mcses = BTreeSet::from_iter(vec![
-            BTreeSet::from_iter(vec![res.soft_constrs_reifs.get_reif_lit(2, 0)]),
+            BTreeSet::from_iter(vec![soft_constrs_reif_lits[2].unwrap()]),
             BTreeSet::from_iter(vec![
-                res.soft_constrs_reifs.get_reif_lit(0, 0),
-                res.soft_constrs_reifs.get_reif_lit(3, 0),
+                soft_constrs_reif_lits[0].unwrap(),
+                soft_constrs_reif_lits[3].unwrap(),
             ]),
             BTreeSet::from_iter(vec![
-                res.soft_constrs_reifs.get_reif_lit(1, 0),
-                res.soft_constrs_reifs.get_reif_lit(3, 0),
+                soft_constrs_reif_lits[1].unwrap(),
+                soft_constrs_reif_lits[3].unwrap(),
             ]),
         ]);
 
-        assert_eq!(computed_muses, expected_muses);
-        assert_eq!(computed_mcses, expected_mcses);
+        assert_eq!(res_muses, expected_muses);
+        assert_eq!(res_mcses, expected_mcses);
     }
 }
--- explainability/src/musmcs_enumeration/marco/simple_marco.rs
+++ explainability/src/musmcs_enumeration/marco/simple_marco.rs
@@ -1,4 +1,3 @@
-use crate::musmcs_enumeration::SoftConstraintsReifications;
 use aries::backtrack::Backtrack;
 use aries::core::{Lit, INT_CST_MAX};
 use aries::model::extensions::AssignmentExt;
@@ -12,57 +11,62 @@ use aries::solver::Solver;
 use itertools::Itertools;
 
 use super::{MapSolver, Marco, MusMcsEnumerationConfig, MusMcsEnumerationResult, SubsetSolver};
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 use std::sync::Arc;
 
-fn create_solver<Lbl: Label>(model: Model<Lbl>) -> Solver<Lbl> {
-    let stn_config = StnConfig {
-        theory_propagation: TheoryPropagationLevel::Full,
-        ..Default::default()
-    };
-    let mut solver = Solver::<Lbl>::new(model);
-    solver.reasoners.diff.config = stn_config;
-    solver
+struct SimpleMapSolver {
+    s: Solver<u8>,
+    literals: BTreeSet<Lit>,
+    to_max: IAtom,
+    lits_translate_in: BTreeMap<Lit, Lit>,  // Needed to translate / map the soft constraint reification literals in
+    lits_translate_out: BTreeMap<Lit, Lit>, // the subset solver to their counterparts in the map solver (this one).
 }
 
-struct SimpleMapSolver<Lbl: Label> {
-    s: Solver<Lbl>,
-    soft_constrs_reif_lits: Arc<BTreeSet<Lit>>,
-    to_maximize: IAtom,
-}
+impl SimpleMapSolver {
+    fn new(literals: Vec<Lit>) -> Self {
+        let mut model = Model::new();
+        let mut lits_translate_in = BTreeMap::<Lit, Lit>::new();
+        let mut lits_translate_out = BTreeMap::<Lit, Lit>::new(); 
 
-impl<Lbl: Label> SimpleMapSolver<Lbl> {
-    fn new(mut model: Model<Lbl>, soft_constrs_reif_lits: Arc<BTreeSet<Lit>>) -> Self {
-        model.enforce(or((*soft_constrs_reif_lits).clone().into_iter().collect_vec()), []);
+        for literal in literals {
+            let lit = model.state.new_var(0, 1).geq(1);
+            lits_translate_in.insert(literal, lit);
+            lits_translate_out.insert(lit, literal);
+        }
 
-        let to_maximize = IAtom::from(model.state.new_var(0, INT_CST_MAX));
-        let sum_ = LinearSum::of(
-            (*soft_constrs_reif_lits)
-                .clone()
-                .into_iter()
-                .map(|l| IAtom::from(l.variable()))
+        let literals: BTreeSet<Lit> = lits_translate_out.keys().cloned().collect();
+ 
+        let to_max = IAtom::from(model.state.new_var(0, INT_CST_MAX));
+        let literals_sum = LinearSum::of(
+            literals
+                .iter()
+                .map(|&l| IAtom::from(l.variable()))
                 .collect_vec(),
         );
-        model.enforce(sum_.clone().leq(to_maximize), []);
-        model.enforce(sum_.clone().geq(to_maximize), []);
+        model.enforce(literals_sum.clone().leq(to_max), []);
+        model.enforce(literals_sum.geq(to_max), []);
 
-        SimpleMapSolver::<Lbl> {
-            s: create_solver(model),
-            soft_constrs_reif_lits,
-            to_maximize,
+        model.enforce(or(literals.iter().cloned().collect_vec()), []);
+
+        SimpleMapSolver {
+            s: Solver::<u8>::new(model),
+            literals,
+            to_max,
+            lits_translate_in,
+            lits_translate_out,
         }
     }
 }
 
-impl<Lbl: Label> MapSolver<Lbl> for SimpleMapSolver<Lbl> {
+impl MapSolver for SimpleMapSolver {
     fn find_unexplored_seed(&mut self) -> Option<BTreeSet<Lit>> {
-        match self.s.maximize(self.to_maximize).unwrap() {
+        match self.s.maximize(self.to_max).unwrap() {
             Some((_, best_assignment)) => {
                 let seed = Some(
-                    self.soft_constrs_reif_lits
+                    self.literals
                         .iter()
                         .filter(|&l| best_assignment.entails(*l))
-                        .cloned()
+                        .map(|l| self.lits_translate_out[l])
                         .collect(),
                 );
                 self.s.reset();
@@ -76,32 +80,43 @@ impl<Lbl: Label> MapSolver<Lbl> for SimpleMapSolver<Lbl> {
     }
 
     fn block_down(&mut self, frompoint: &BTreeSet<Lit>) {
-        let complement = self.soft_constrs_reif_lits.difference(frompoint).cloned().collect_vec();
+        let complement = self
+            .literals
+            .difference(&frompoint.iter().map(|&l| self.lits_translate_in[&l]).collect())
+            .cloned()
+            .collect_vec();
         self.s.enforce(or(complement), []);
     }
 
     fn block_up(&mut self, frompoint: &BTreeSet<Lit>) {
-        let neg = frompoint.iter().map(|&l| !l).collect_vec();
+        let neg = frompoint.iter().map(|&l| !self.lits_translate_in[&l]).collect_vec();
         self.s.enforce(or(neg), []);
     }
 
-    fn get_internal_solver(&mut self) -> &mut Solver<Lbl> {
-        &mut self.s
-    }
+    // fn get_internal_solver(&mut self) -> &mut Solver<u8> {
+    //     &mut self.s
+    // }
 }
 
 struct SimpleSubsetSolver<Lbl: Label> {
     s: Solver<Lbl>,
     soft_constrs_reif_lits: Arc<BTreeSet<Lit>>,
-    last_unsat_core: BTreeSet<Lit>,
+    cached_unsat_core: BTreeSet<Lit>,
 }
 
 impl<Lbl: Label> SimpleSubsetSolver<Lbl> {
     fn new(model: Model<Lbl>, soft_constrs_reif_lits: Arc<BTreeSet<Lit>>) -> Self {
+        let stn_config = StnConfig {
+            theory_propagation: TheoryPropagationLevel::Full,
+            ..Default::default()
+        };
+        let mut s = Solver::<Lbl>::new(model);
+        s.reasoners.diff.config = stn_config;
+
         SimpleSubsetSolver::<Lbl> {
-            s: create_solver(model),
+            s,
             soft_constrs_reif_lits,
-            last_unsat_core: BTreeSet::new(),
+            cached_unsat_core: BTreeSet::new(),
         }
     }
 }
@@ -111,12 +126,12 @@ impl<Lbl: Label> SubsetSolver<Lbl> for SimpleSubsetSolver<Lbl> {
         // FIXME warm-start / solution hints optimization should go here... right ?
         let res = self
             .s
-            .solve_with_assumptions(seed.clone())
+            .solve_with_assumptions(seed.into_iter().cloned())
             .expect("Solver interrupted...");
         self.s.reset();
         if let Err(unsat_core) = res {
-            self.last_unsat_core.clear();
-            self.last_unsat_core.extend(unsat_core.literals());
+            self.cached_unsat_core.clear();
+            self.cached_unsat_core.extend(unsat_core.literals());
             false
         } else {
             true
@@ -124,119 +139,118 @@ impl<Lbl: Label> SubsetSolver<Lbl> for SimpleSubsetSolver<Lbl> {
     }
 
     fn grow(&mut self, seed: &BTreeSet<Lit>) -> (BTreeSet<Lit>, Option<BTreeSet<Lit>>) {
-        let mut mss_lits = seed.clone();
-        for &lit in (*self.soft_constrs_reif_lits).clone().difference(seed) {
-            mss_lits.insert(lit);
-            if !self.check_seed_sat(&mss_lits) {
-                mss_lits.remove(&lit);
+        let mut mss = seed.clone();
+        for &lit in self.soft_constrs_reif_lits.clone().difference(seed) {
+            mss.insert(lit);
+            if !self.check_seed_sat(&mss) {
+                mss.remove(&lit);
             }
         }
-        let mcs_lits: BTreeSet<Lit> = self.soft_constrs_reif_lits.difference(&mss_lits).cloned().collect();
-        (mss_lits, Some(mcs_lits))
+        let mcs: BTreeSet<Lit> = self.soft_constrs_reif_lits.difference(&mss).cloned().collect();
+        (mss, Some(mcs))
     }
 
     fn shrink(&mut self, seed: &BTreeSet<Lit>) -> BTreeSet<Lit> {
-        let mut mus_lits: BTreeSet<Lit> = seed.clone();
+        let mut mus: BTreeSet<Lit> = seed.clone();
         for &lit in seed {
-            if !mus_lits.contains(&lit) {
+            if !mus.contains(&lit) {
                 continue;
             }
-            mus_lits.remove(&lit);
-            if !self.check_seed_sat(&mus_lits) {
-                mus_lits = self.last_unsat_core.clone();
+            mus.remove(&lit);
+            if !self.check_seed_sat(&mus) {
+                mus = self.cached_unsat_core.clone();
             } else {
-                debug_assert!(!mus_lits.contains(&lit));
-                mus_lits.insert(lit);
+                debug_assert!(!mus.contains(&lit));
+                mus.insert(lit);
             }
         }
-        mus_lits
+        mus
     }
 
-    fn get_internal_solver(&mut self) -> &mut Solver<Lbl> {
-        &mut self.s
-    }
+    // fn get_internal_solver(&mut self) -> &mut Solver<Lbl> {
+    //     &mut self.s
+    // }
 }
 
 pub struct SimpleMarco<Lbl: Label> {
-    soft_constrs_reif_lits: Arc<BTreeSet<Lit>>,
-    config: MusMcsEnumerationConfig,
-    map_solver: SimpleMapSolver<Lbl>,
-    subset_solver: SimpleSubsetSolver<Lbl>,
-    result: MusMcsEnumerationResult<Lbl>,
+//    config: MusMcsEnumerationConfig,
+    cached_result: MusMcsEnumerationResult,
     seed: BTreeSet<Lit>,
+    map_solver: SimpleMapSolver,
+    subset_solver: SimpleSubsetSolver<Lbl>,
+    soft_constrs_reif_lits: Arc<BTreeSet<Lit>>,
 }
 
 impl<Lbl: Label> Marco<Lbl> for SimpleMarco<Lbl> {
-    fn new<Expr: Reifiable<Lbl> + Copy>(
+    fn new_with_soft_constrs_reif_lits(
         model: Model<Lbl>,
-        soft_constrs: Vec<Expr>,
+        soft_constrs_reifs_lits: Vec<Lit>,
         config: MusMcsEnumerationConfig,
     ) -> Self {
-        let mut map_model = model.clone();
-        let mut subset_model = model.clone();
-
-        let mapping = soft_constrs
-            .clone()
-            .into_iter()
-            .map(|_| {
-                let _a = map_model.state.new_var(0, 1).geq(1);
-                let _b = subset_model.state.new_var(0, 1).geq(1);
-                debug_assert_eq!(_a, _b);
-                _b
-            })
-            .collect_vec();
-        let soft_constrs_reif_lits = Arc::new(mapping.clone().into_iter().collect::<BTreeSet<Lit>>());
-
-        for (soft_constr_reif_lit, soft_constr) in soft_constrs_reif_lits.iter().zip_eq(soft_constrs) {
-            subset_model.bind(soft_constr, *soft_constr_reif_lit);
-        }
-        let result = MusMcsEnumerationResult::<Lbl> {
-            soft_constrs_reifs: SoftConstraintsReifications {
-                models: Arc::new(vec![subset_model.clone()]),
-                mapping,
-            },
-            muses_reif_lits: if config.return_muses {
+        
+        let cached_result = MusMcsEnumerationResult {
+            muses: if config.return_muses {
                 Some(Vec::<BTreeSet<Lit>>::new())
             } else {
                 None
             },
-            mcses_reif_lits: if config.return_mcses {
+            mcses: if config.return_mcses {
                 Some(Vec::<BTreeSet<Lit>>::new())
             } else {
                 None
             },
         };
-        debug_assert_eq!(result.muses_reif_lits.is_some(), config.return_muses);
-        debug_assert_eq!(result.mcses_reif_lits.is_some(), config.return_mcses);
+        debug_assert_eq!(cached_result.muses.is_some(), config.return_muses);
+        debug_assert_eq!(cached_result.mcses.is_some(), config.return_mcses);
 
-        let map_solver = SimpleMapSolver::<Lbl>::new(map_model, soft_constrs_reif_lits.clone());
-        let subset_solver = SimpleSubsetSolver::<Lbl>::new(subset_model, soft_constrs_reif_lits.clone());
+        let map_solver = SimpleMapSolver::new(soft_constrs_reifs_lits.clone());
+
+        let soft_constrs_reif_lits = Arc::new(
+            BTreeSet::from_iter(soft_constrs_reifs_lits.into_iter())
+        );
+
+        let subset_solver = SimpleSubsetSolver::<Lbl>::new(model, soft_constrs_reif_lits.clone());
 
         Self {
-            soft_constrs_reif_lits,
-            config,
+//            config,
+            cached_result,
+            seed: BTreeSet::new(),
             map_solver,
             subset_solver,
-            result,
-            seed: BTreeSet::new(),
+            soft_constrs_reif_lits,
         }
+    }
+
+    fn new_with_soft_constrs<Expr: Reifiable<Lbl>>(
+        model: Model<Lbl>,
+        soft_constrs: Vec<Expr>,
+        config: MusMcsEnumerationConfig,
+    ) -> Self {
+
+        let mut model = model.clone();
+        let soft_constrs_reif_lits = soft_constrs
+            .into_iter()
+            .map(|expr| model.reify(expr))
+            .collect_vec();
+
+        Self::new_with_soft_constrs_reif_lits(model, soft_constrs_reif_lits, config)
     }
 
     fn reset_result(&mut self) {
-        if let Some(ref mut v) = self.result.muses_reif_lits {
-            v.clear();
+        if let Some(ref mut muses) = self.cached_result.muses {
+            muses.clear();
         }
-        if let Some(ref mut v) = self.result.mcses_reif_lits {
-            v.clear();
+        if let Some(ref mut mcses) = self.cached_result.mcses {
+            mcses.clear();
         }
     }
 
-    fn clone_result(&self) -> MusMcsEnumerationResult<Lbl> {
-        self.result.clone()
+    fn clone_result(&self) -> MusMcsEnumerationResult {
+        self.cached_result.clone()
     }
 
-    fn get_result(&self) -> &MusMcsEnumerationResult<Lbl> {
-        &self.result
+    fn get_expr_reif_lit<Expr: Reifiable<Lbl>>(&mut self, soft_constr: Expr) -> Result<Lit, ()> {
+        self.subset_solver.s.model.check_reified(soft_constr)
     }
 
     fn find_unexplored_seed(&mut self) -> bool {
@@ -254,43 +268,53 @@ impl<Lbl: Label> Marco<Lbl> for SimpleMarco<Lbl> {
     }
 
     fn do_case_seed_sat(&mut self) {
-        if let Some(ref mut v) = self.result.mcses_reif_lits {
-            let (mss_lits, mcs_lits) = self.subset_solver.grow(&self.seed);
-            self.map_solver.block_down(&mss_lits);
-            v.push(mcs_lits.unwrap());
+        if let Some(ref mut mcses) = self.cached_result.mcses {
+            let (mss, mcs) = self.subset_solver.grow(&self.seed);
+            self.map_solver.block_down(&mss);
+            mcses.push(mcs.unwrap());
         } else {
             // from Ignace Bleukx's implementation:
             // find more MCSes, **disjoint** from this one, similar to "optimal_mus" in mus.py
             // can only be done when MCSes do not have to be returned as there is no guarantee
             // the MCSes encountered during enumeration are "new" MCSes
-            let mut sat_subset_lits: BTreeSet<Lit> = (*self.soft_constrs_reif_lits)
-                .clone()
-                .into_iter()
-                .filter(|&l| self.subset_solver.s.model.entails(!l))
+            let mut sat_subset_lits: BTreeSet<Lit> = self.soft_constrs_reif_lits
+                .iter()
+                .filter(|&&l| self.subset_solver.s.model.entails(!l))
+                .cloned()
                 .collect();
             self.map_solver
-                .get_internal_solver()
-                .model
-                .enforce(or(sat_subset_lits.clone().into_iter().collect_vec()), []);
+                .s
+                .enforce(or(
+                        sat_subset_lits
+                        .iter()
+                        .map(|&l| self.map_solver.lits_translate_in[&l])
+                        .collect_vec()
+                    ), []);
             while self.subset_solver.check_seed_sat(&sat_subset_lits) {
                 let s2: BTreeSet<Lit> = self
                     .soft_constrs_reif_lits
                     .iter()
-                    .filter(|&l| self.subset_solver.s.model.entails(*l))
+                    .filter(|&&l| self.subset_solver.s.model.entails(l))
                     .cloned()
                     .collect();
-                let new_mcs_lits = self.soft_constrs_reif_lits.difference(&s2).cloned();
-                sat_subset_lits.extend(new_mcs_lits.clone());
-                self.map_solver.s.model.enforce(or(new_mcs_lits.collect_vec()), []);
+                let new_mcs = self.soft_constrs_reif_lits.difference(&s2).cloned();
+                sat_subset_lits.extend(new_mcs.clone());
+                self.map_solver.s
+                .enforce(or(
+                    new_mcs
+                    .map(|l| self.map_solver.lits_translate_in[&l])
+                    .collect_vec()
+                ), []);
+
             }
         }
     }
 
     fn do_case_seed_unsat(&mut self) {
-        let mus_lits = self.subset_solver.shrink(&self.seed);
-        self.map_solver.block_up(&mus_lits);
-        if let Some(ref mut v) = self.result.muses_reif_lits {
-            v.push(mus_lits);
+        let mus = self.subset_solver.shrink(&self.seed);
+        self.map_solver.block_up(&mus);
+        if let Some(ref mut muses) = self.cached_result.muses {
+            muses.push(mus);
         }
     }
 }
