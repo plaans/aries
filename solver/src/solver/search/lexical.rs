d9d205121ebc9c1e0e77f784a1a0ec17ec0ddd1a
Nika Beriachvili <nikaberiachvilipro@gmail.com>
2025-01-11 14:50:40 +0100

    feat(branching): add optionally specifying a subset of vars to the Lexical brancher

--- solver/src/solver/search/lexical.rs
+++ solver/src/solver/search/lexical.rs
@@ -1,4 +1,7 @@
+use std::collections::BTreeSet;
+
 use crate::backtrack::{Backtrack, DecLvl, DecisionLevelTracker};
+use crate::core::VarRef;
 use crate::model::extensions::AssignmentExt;
 use crate::model::Model;
 use crate::solver::search::{Decision, SearchControl};
@@ -12,8 +15,9 @@ pub enum PreferredValue {
 
 /// Assigns all values in lexical order to their minimal or maximal value.
 /// Essentially intended to finish the search once all high-priority variables have been set.
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 pub struct Lexical {
+    vars: Option<BTreeSet<VarRef>>,
     pref: PreferredValue,
     lvl: DecisionLevelTracker,
 }
@@ -21,6 +25,15 @@ pub struct Lexical {
 impl Lexical {
     pub fn new(preferred_value: PreferredValue) -> Self {
         Lexical {
+            vars: None,
+            pref: preferred_value,
+            lvl: Default::default(),
+        }
+    }
+
+    pub fn with_vars(vars: impl IntoIterator<Item = VarRef>, preferred_value: PreferredValue) -> Self {
+        Lexical {
+            vars: Some(vars.into_iter().collect()),
             pref: preferred_value,
             lvl: Default::default(),
         }
@@ -58,7 +71,13 @@ impl<L> SearchControl<L> for Lexical {
             .state
             .variables()
             .filter_map(|v| {
-                if model.state.present(v) == Some(true) {
+                let cond = if let Some(vars) = &self.vars {
+                    vars.contains(&v)
+                } else {
+                    true
+                }.then_some(model.state.present(v) == Some(true)).unwrap_or(false);
+
+                if cond {
                     let dom = model.var_domain(v);
                     if dom.is_bound() {
                         None
@@ -76,6 +95,6 @@ impl<L> SearchControl<L> for Lexical {
     }
 
     fn clone_to_box(&self) -> Box<dyn SearchControl<L> + Send> {
-        Box::new(*self)
+        Box::new(self.clone())
     }
 }
