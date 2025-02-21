b7c627213680b0c171617d653d045dcaad04ae8d
Titouan Seraud <titouan.seraud@laas.fr>
2025-02-21 10:29:40 +0100

    Added transitive conversion macros.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod domain;
+pub mod macros;
 pub mod model;
 pub mod solve;
 pub mod types;
---
+++ src/macros.rs
@@ -0,0 +1,44 @@
+/// Given three types A, B and C with the following traits:
+/// - From<B> for A, From<C> for B,
+/// - TryFrom<A> for B, TryFrom<B> for C
+///
+/// The macro implements the traits:
+///  - From<C> for A
+///  - TryFrom<A> for C
+#[macro_export]
+macro_rules! transitive_conversions {
+    ($A: ty, $B: ty, $C: ty) => {
+        impl From<$C> for $A {
+            fn from(i: $C) -> Self {
+                <$B>::from(i).into()
+            }
+        }
+
+        impl TryFrom<$A> for $C {
+            type Error = ConversionError;
+
+            fn try_from(value: $A) -> Result<Self, Self::Error> {
+                match <$B>::try_from(value) {
+                    Ok(x) => <$C>::try_from(x),
+                    Err(x) => Err(x),
+                }
+            }
+        }
+    };
+}
+
+/// Given three types A, B and C with the following traits:
+/// - From<B> for A, From<C> for B,
+///
+/// The macro implements the traits:
+///  - From<C> for A
+#[macro_export]
+macro_rules! transitive_conversion {
+    ($A: ty, $B: ty, $C: ty) => {
+        impl From<$C> for $A {
+            fn from(i: $C) -> Self {
+                <$B>::from(i).into()
+            }
+        }
+    };
+}
