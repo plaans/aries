40da105cdf615572924a0377ab8fe1da403f2edf
Roland Godet <r.godet@outlook.fr>
2025-03-03 08:47:38 +0100

    test(warm-up): preparation for other domains

--- planning/unified/plugin/test/test_warm_up.py
+++ planning/unified/plugin/test/test_warm_up.py
@@ -3,8 +3,10 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 # pylint: disable=too-few-public-methods, redefined-outer-name
 
+from dataclasses import dataclass
 import os
 from pathlib import Path
+from typing import Generator
 
 import pytest
 from unified_planning.io.pddl_reader import PDDLReader
@@ -12,33 +14,34 @@ from unified_planning.plans.plan import Plan
 from unified_planning.shortcuts import OneshotPlanner, Problem
 
 
-@pytest.fixture
-def domain_file() -> Path:
-    return Path(__file__).parent / "fixtures/warm_up/domain.pddl"
+@dataclass(frozen=True)
+class WarmUpScenario:
+    uid: str
+    problem: Problem
+    plan: Plan
+
+    def __iter__(self):
+        return iter((self.problem, self.plan))
 
 
-@pytest.fixture
-def problem_file() -> Path:
-    return Path(__file__).parent / "fixtures/warm_up/problem.pddl"
+def _scenarios() -> Generator[WarmUpScenario, None, None]:
+    fixtures_dir = Path(__file__).parent / "fixtures/warm_up"
+    for domain_dir in fixtures_dir.iterdir():
+        if not domain_dir.is_dir():
+            continue
+        domain_file = domain_dir / "domain.pddl"
+        problem_file = domain_dir / "problem.pddl"
+        for plan_file in domain_dir.glob("plan_*.txt"):
+            uid = f"{domain_dir.name}/{plan_file.stem}"
+            problem = PDDLReader().parse_problem(domain_file, problem_file)
+            plan = PDDLReader().parse_plan(problem, plan_file)
+            yield WarmUpScenario(uid=uid, problem=problem, plan=plan)
 
 
-@pytest.fixture
-def plan_file() -> Path:
-    return Path(__file__).parent / "fixtures/warm_up/plan_76.txt"
-
-
-@pytest.fixture
-def problem(domain_file: Path, problem_file: Path) -> Problem:
-    return PDDLReader().parse_problem(domain_file, problem_file)
-
-
-@pytest.fixture
-def plan(plan_file: Path, problem: Problem) -> Plan:
-    return PDDLReader().parse_plan(problem, plan_file)
-
-
+@pytest.mark.parametrize("scenario", _scenarios(), ids=lambda tc: tc.uid)
 class TestAriesWarmUp:
-    def test_oneshot_with_warm_up_returns_same_plan(self, problem: Problem, plan: Plan):
+    def test_oneshot_with_warm_up_returns_same_plan(self, scenario: WarmUpScenario):
+        problem, plan = scenario
         os.environ["ARIES_UP_ASSUME_REALS_ARE_INTS"] = "true"
         os.environ["ARIES_LCP_SYMMETRY_BREAKING"] = "simple"
         with OneshotPlanner(name="aries", params={"warm_up_plan": plan}) as planner:
