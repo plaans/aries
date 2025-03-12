b55c6549fa8f9f684dfe50f8749f553a3e411219
Arthur Bit-Monnot <abitmonnot@laas.fr>
2025-03-12 17:19:57 +0100

    fix(symmetry): ensure that conditions are grouped by template
    Without this, constructing the instances (hence with strange instance_ids) could rule out some valid solutions

--- planning/planners/src/encode/symmetry.rs
+++ planning/planners/src/encode/symmetry.rs
@@ -193,28 +193,37 @@ fn add_plan_space_symmetry_breaking(pb: &FiniteProblem, model: &mut Model, encod
         }
     };
 
-    let sort = |conds: HashSet<CondID>| {
-        if sort_by_hierarchy_level {
-            let sort_key = |c: &CondID| {
-                // penalize conditions on numeric fluents because the encoding
-                // makes it hard to distinguish actual supports, they should be considered last
-                let penalty = if penalize_numeric_supports && is_num(c) {
-                    1024
+    let sort = |conds: BTreeSet<CondID>| {
+        let sort_key = |c: &CondID| {
+            // penalize conditions on numeric fluents because the encoding
+            // makes it hard to distinguish actual supports, they should be considered last
+            let penalty = if penalize_numeric_supports && is_num(c) {
+                1024
+            } else {
+                0
+            };
+            // get the level, reserving the lvl 0 for non-templates
+            let (hier_level, template) = if let Some(template) = template_id(c.instance_id) {
+                let lvl = if sort_by_hierarchy_level {
+                    pb.meta.action_hierarchy[&template]
                 } else {
                     0
                 };
-                // get the level, reserving the lvl 0 for non-templates
-                if let Some(template) = template_id(c.instance_id) {
-                    let lvl = pb.meta.action_hierarchy[&template];
-                    (penalty + lvl + 1, c.instance_id, c.cond_id)
-                } else {
-                    (penalty, c.instance_id, c.cond_id)
-                }
+                (lvl + 1, template + 1)
+            } else {
+                (0, 0)
             };
-            conds.into_iter().sorted_by_cached_key(sort_key).collect_vec()
-        } else {
-            conds.into_iter().sorted().collect_vec()
-        }
+            let generation = if let Some(generation) = generation_id(c.instance_id) {
+                generation + 1000
+            } else {
+                c.instance_id
+            };
+            // important: order must be made by template so that conditions of the same template are grouped
+            // The consequence of grouping is that swapping raws (instances of the same template) does not affect the order with respect to the columns of the other templates
+            // note that two instances of the same template will have the same hierarchy level, so it is safe to first group by hierarchy level
+            (hier_level, penalty, template, generation)
+        };
+        conds.into_iter().sorted_by_key(sort_key).collect_vec()
     };
     let conds_by_templates: BTreeMap<TemplateID, Vec<CondID>> =
         conds_by_templates.into_iter().map(|(k, v)| (k, sort(v))).collect();
