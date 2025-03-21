a643a2ea927c355725902bfff20f12336f7d082e
Titouan Seraud <titouan.seraud@laas.fr>
2025-03-21 10:11:46 +0100

    Updated transitive dependency to have its issue 11 fixed.

--- src/fzn/parvar.rs
+++ src/fzn/parvar.rs
@@ -7,18 +7,13 @@ use crate::fzn::var::Var;
 use crate::fzn::var::VarBool;
 use crate::fzn::var::VarInt;
 
-// Workaround to transitive crate issue
-// https://github.com/bobozaur/transitive/issues/11
-type RcIntVariable = Rc<VarInt>;
-type RcBoolVariable = Rc<VarBool>;
-
 #[derive(Transitive)]
 #[transitive(from(Rc<VarBool>, Var))]
 #[transitive(from(VarBool, Rc<VarBool>, Var))]
 #[transitive(from(Rc<VarInt>, Var))]
 #[transitive(from(VarInt, Rc<VarInt>, Var))]
-#[transitive(try_into(Var, RcIntVariable))]
-#[transitive(try_into(Var, RcBoolVariable))]
+#[transitive(try_into(Var, Rc<VarInt>))]
+#[transitive(try_into(Var, Rc<VarBool>))]
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum ParVar {
     Par(Par),
