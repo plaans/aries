066e998248b6819fd8b3afe64a5ea210a9ff04ca
Nika Beriachvili <nikaberiachvilipro@gmail.com>
2025-04-14 14:12:45 +0200

    chore(beluga): update beluga io

--- planning/beluga_study/src/io.rs
+++ planning/beluga_study/src/io.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use serde_json::{self, json};
 use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Clone, Parser)]
@@ -32,6 +35,30 @@ pub struct ExplainArgs {
     // #[clap(flatten)]
     // pub problem_file: ProblemFile,
     pub problem_file_path: String,
-    pub question_name: String,
-    pub question_args: Vec<String>,
+    pub results_file_path: String,
+}
+
+pub fn write_mus_mcs_enumeration_result_to_file(
+    results_file_path: String,
+    complete: Option<bool>,
+    muses: Vec<Vec<String>>,
+    mcses: Vec<Vec<String>>,
+) -> std::io::Result<()> {
+
+    // let _ = create_dir_all(&results_file_path)?;
+    let file = File::create_new(&results_file_path).or_else(|_| File::create(&results_file_path))?;
+    let mut writer = BufWriter::new(file);
+
+    serde_json::to_writer(
+        &mut writer,
+        &json!({
+            "complete": complete.as_ref(),
+            "muses": muses,
+            "mcses": mcses,
+        }),
+    )?;
+
+    writer.flush()?;
+    Ok(())
+}
 }
