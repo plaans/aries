73bc74418199378e48ab37c0deab15062fa5c223
Titouan Seraud <titouan.seraud@laas.fr>
2025-05-19 09:55:41 +0200

    Flatzinc Int type is now equal to aries IntCst.

--- aries_fzn/src/fzn/types.rs
+++ aries_fzn/src/fzn/types.rs
@@ -1,11 +1,13 @@
 //! Flatzinc types.
 
+use aries::core::IntCst;
+
 /// Flatzinc integer value.
 ///
 /// ```flatzinc
 /// int: p = 14;
 /// ```
-pub type Int = i32;
+pub type Int = IntCst;
 
 /// Convert the given boolean to [Int].
 pub fn as_int(b: bool) -> Int {
