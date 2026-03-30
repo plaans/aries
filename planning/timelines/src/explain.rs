cd43f6450e45b6cbea17d0df878ba7c1ccbf128b
Arthur Bit-Monnot <abitmonnot@laas.fr>
2026-03-30 18:17:31 +0200

    feat(timelines): add optimization interface to explanable solver.

--- planning/timelines/src/explain.rs
+++ planning/timelines/src/explain.rs
@@ -1,5 +1,6 @@
 use std::collections::{BTreeMap, BTreeSet};
 
+use aries::model::lang::FAtom;
 use aries::prelude::*;
 use aries::{
     backtrack::Backtrack,
@@ -57,9 +58,21 @@ impl<T: Ord + Clone> ExplainableSolver<T> {
             .solve_with_assumptions(&assumptions, aries::solver::SearchLimit::None)
             .unwrap()
             .ok();
-        self.solver.print_stats();
+        // self.solver.print_stats();
         self.solver.reset(); // TODO: this should not be needed
         res
+    }
+    ///
+    /// Check if the model is satifiable with all assumptions, and returns a solution if it is.
+    pub fn find_optimal(&mut self, obj: FAtom, on_new_solution: impl FnMut(&Solution)) -> Option<Solution> {
+        let assumptions = self.enablers.keys().copied().collect_vec();
+        let res = self
+            .solver
+            .minimize_with_assumptions(obj.num, &assumptions, aries::solver::SearchLimit::None, on_new_solution)
+            .unwrap();
+        // self.solver.print_stats();
+        self.solver.reset(); // TODO: this should not be needed
+        res.ok()
     }
 
     /// Returns an iterator over all MUS and MCS in the model.
@@ -69,6 +82,14 @@ impl<T: Ord + Clone> ExplainableSolver<T> {
         self.solver
             .mus_and_mcs_enumerator(&assumptions)
             .map(move |mm| mm.project(projection))
+    }
+
+    /// Returns an iterator over all MUS (Minimal Unsatifiable Subsets) in the model.
+    pub fn muses(&mut self) -> impl Iterator<Item = BTreeSet<T>> + '_ {
+        self.explain_unsat().filter_map(|mus_mcs| match mus_mcs {
+            MusMcs::Mus(mus) => Some(mus),
+            MusMcs::Mcs(_) => None,
+        })
     }
 
     /// Returns the smallest MCS over all assumptions
