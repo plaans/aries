d7baf2202f989d02d4ac92ad0673291d17c34a20
Nika Beriachvili <nikaberiachvilipro@gmail.com>
2026-05-01 22:20:41 +0200

    chore(ilp): refactor ilp [no ci]

--- examples/ilp/src/main.rs
+++ examples/ilp/src/main.rs
@@ -1,12 +1,12 @@
 mod problem;
 
-use anyhow::*;
 use aries::model::extensions::Shaped;
 use aries::prelude::*;
 use aries::solver::{Exit, SearchLimit};
+use aries_bench_data::IntermediateResult;
 use aries_lprelax::{LpRelax, default_lit_implications, default_lplit_implications};
 use clap::Parser;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::path::PathBuf;
 use std::time::{Duration, Instant};
 
@@ -27,17 +27,34 @@ struct Cli {
     /// Timeout (seconds)
     #[arg(long, short)]
     timeout: Option<u64>,
+
+    #[arg(long)]
+    expected_objective: Option<IntCst>,
+
+    #[arg(long, default_value_t = false)]
+    expected_unsat: bool,
 }
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    let input_file = std::fs::read_to_string(&cli.file)?;
+    assert!(
+        !(cli.expected_objective.is_some() && cli.expected_unsat),
+        "Cannot expect UNSAT and an objective value at the same time."
+    );
+
+    let problem_string = std::fs::read_to_string(&cli.file)?;
     let use_lprelax = !cli.no_lprelax;
 
+    let problem_info = aries_bench_data::Problem {
+        name: cli.file.to_string_lossy().to_string(),
+        timeout: Duration::from_secs(cli.timeout.unwrap_or_default()),
+        flags: BTreeMap::new(),
+    };
+
     let problem = match &cli.file.extension().and_then(std::ffi::OsStr::to_str) {
-        Some("mps") => IlpProblem::from_mps(&input_file)?,
-        Some("lp") => IlpProblem::from_lp(&input_file)?,
+        Some("mps") => IlpProblem::from_mps(&problem_string)?,
+        Some("lp") => IlpProblem::from_lp(&problem_string)?,
         _ => return Err(anyhow::anyhow!("Input file needs to be .mps or .lp")),
     };
     // println!("{:#?}", problem);
@@ -46,32 +63,116 @@ fn main() -> Result<()> {
 
     let mut solver = make_solver(&problem, model, use_lprelax);
 
-    let limit = if let Some(timeout) = cli.timeout {
-        SearchLimit::Deadline(Instant::now() + Duration::from_secs(timeout))
-    } else {
-        SearchLimit::None
+    solve(
+        &problem,
+        &mut solver,
+        cli.timeout,
+        cli.expected_objective,
+        cli.expected_unsat,
+        problem_info,
+    )
+}
+
+fn solve(
+    problem: &IlpProblem,
+    solver: &mut Solver,
+    timeout: Option<u64>,
+    expected_objective: Option<IntCst>,
+    expected_unsat: bool,
+    problem_info: aries_bench_data::Problem,
+) -> anyhow::Result<()> {
+    let start_time = Instant::now();
+    let deadline = timeout
+        .map(|dur| SearchLimit::Deadline(start_time + Duration::from_secs(dur)))
+        .unwrap_or(SearchLimit::None);
+
+    let objective = problem
+        .obj
+        .as_ref()
+        .and_then(|(obj_name, _)| solver.get_int_var(obj_name))
+        .unwrap_or(VarRef::ZERO.into());
+
+    let mut solution_history: Vec<IntermediateResult> = Default::default();
+    let mut best = Option::None;
+
+    let on_new_solution = |obj: IntCst, sol: &Solution| {
+        println!("New solution with objective: {}", obj);
+        solution_history.push(IntermediateResult {
+            timestamp: start_time.elapsed(),
+            objective: obj as i64,
+        });
+        best = Some(sol.clone());
     };
-    let res = solve(&problem, &mut solver, limit)?;
 
+    let result = match problem.sense {
+        lp_parser_rs::model::Sense::Minimize => solver.minimize_with_callback(objective, on_new_solution, deadline),
+        lp_parser_rs::model::Sense::Maximize => solver.maximize_with_callback(objective, on_new_solution, deadline),
+    };
     solver.print_stats();
-    //solver.model.print_state();
+    println!();
 
-    if let Some((obj, sol)) = res {
-        println!("obj = {}", obj);
+    let status = match result {
+        Ok(Some((obj, solution))) => {
+            let optimum = solution.value_of(objective).unwrap();
+            assert_eq!(obj, optimum); // sanity check
+            println!("> OPTIMAL (objective: {optimum})");
 
-        let mut sol = sol
-            .bound_variables()
-            .filter_map(|(col_name, val)| solver.get_label(col_name).map(|col_name| (col_name, val)))
-            .collect::<Vec<_>>();
-        sol.sort();
-
-        for (col_name, val) in &sol {
-            println!("{:?} = {}", col_name, val);
+            assert!(!expected_unsat, "Expected an unsatisfiable problem");
+            if let Some(expected) = expected_objective {
+                assert_eq!(
+                    optimum, expected,
+                    "The objective found ({optimum}) is not the expected one ({expected})"
+                );
+            }
+            println!(
+                "XX\t{}\t{}\t{}",
+                problem_info.name,
+                optimum,
+                start_time.elapsed().as_secs_f64()
+            );
+            aries_bench_data::SolveStatus::Solved
         }
-    } else {
-        println!("No solution !");
-    }
+        Ok(None) => {
+            println!("> UNSATISFIABLE");
+            assert!(expected_objective.is_none(), "Expected a valid solution");
+            aries_bench_data::SolveStatus::Solved
+        }
+        Err(Exit::Interrupted) => match best.as_ref() {
+            Some(sol) => {
+                let best_cost = sol.value_of(objective).unwrap();
+                println!("> TIMEOUT (best solution cost {best_cost})");
+                aries_bench_data::SolveStatus::Timeout
+            }
+            None => {
+                println!("> TIMEOUT (no solution found)");
+                aries_bench_data::SolveStatus::Timeout
+            }
+        },
+    };
 
+    //let result = aries_bench_data::SolveResult {
+    aries_bench_data::SolveResult {
+        problem: problem_info,
+        status,
+        runtime: start_time.elapsed(),
+        objective_value: best.map(|sol| sol.value_of(objective).unwrap() as i64),
+        metrics: Default::default(),
+        objective_history: solution_history,
+    }
+    .with_metric(
+        aries_bench_data::SolverMetric::NumConflicts,
+        solver.stats.num_conflicts as f64,
+    )
+    .with_metric(
+        aries_bench_data::SolverMetric::NumDecisions,
+        solver.stats.num_decisions as f64,
+    )
+    .with_metric(
+        aries_bench_data::SolverMetric::NumDomUpdates,
+        solver.stats.num_dom_updates as f64,
+    );
+
+    println!("TOTAL RUNTIME: {:.6}", start_time.elapsed().as_secs_f64());
     Ok(())
 }
 
@@ -121,23 +222,4 @@ fn make_solver(problem: &IlpProblem, model: Model, use_lp_relax: bool) -> Solver
     };
 
     Solver::with_extra_reasoners(model, extra_reasoners)
-}
-
-fn solve(problem: &IlpProblem, solver: &mut Solver, limit: SearchLimit) -> Result<Option<(i32, Solution)>, Exit> {
-    if let Some((obj_name, _)) = &problem.obj {
-        match problem.sense {
-            lp_parser_rs::model::Sense::Minimize => solver.minimize_with_callback(
-                solver.get_int_var(obj_name).unwrap(),
-                |o, _s| println!("new sol found: obj: {o}"),
-                limit,
-            ),
-            lp_parser_rs::model::Sense::Maximize => solver.maximize_with_callback(
-                solver.get_int_var(obj_name).unwrap(),
-                |o, _s| println!("new sol found: obj: {o}"),
-                limit,
-            ),
-        }
-    } else {
-        solver.solve(limit).map(|sol| sol.map(|sol| (0, sol)))
-    }
 }
