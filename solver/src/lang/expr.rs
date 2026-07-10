208fda68579bee5085db65acc978bdd36e5ffe64
Arthur Bit-Monnot <abitmonnot@laas.fr>
2026-07-10 18:04:20 +0200

    doc(solver): add documentation to main constraints.

--- solver/src/lang/expr.rs
+++ solver/src/lang/expr.rs
@@ -1,6 +1,6 @@
 //! This module provides high-level methods to construct boolean and integer expressions.
 //!
-//! These are all re-exported in `[crate::prelude`].
+//! These are all re-exported in [`crate::prelude`].
 
 use itertools::Itertools;
 
@@ -13,31 +13,41 @@ use crate::prelude::*;
 
 // CAREFUL: any public item here will be re-exported in the `prelude`
 
-pub fn eq(lhs: impl Into<LinSum>, rhs: impl Into<LinSum>) -> LinEq {
-    lhs.into().eq(rhs)
+/// Requires two integer expressions to be equal (`x = y`)
+pub fn eq(x: impl Into<LinSum>, y: impl Into<LinSum>) -> LinEq {
+    x.into().eq(y)
 }
-pub fn neq(lhs: impl Into<LinSum>, rhs: impl Into<LinSum>) -> LinNeq {
-    lhs.into().neq(rhs)
+/// Requires two integer expressions to be different (`x != y`)
+pub fn neq(x: impl Into<LinSum>, y: impl Into<LinSum>) -> LinNeq {
+    x.into().neq(y)
 }
-pub fn leq(lhs: impl Into<LinSum>, rhs: impl Into<LinSum>) -> LinLeq {
-    lhs.into().leq(rhs)
+/// Requires one integer expression to be smaller than or equal to another one (`x <= y`)
+pub fn leq(x: impl Into<LinSum>, y: impl Into<LinSum>) -> LinLeq {
+    x.into().leq(y)
 }
-pub fn geq(lhs: impl Into<LinSum>, rhs: impl Into<LinSum>) -> LinLeq {
-    lhs.into().geq(rhs)
+/// Requires one integer expression to be greater than or equal to another one (`x >= y`)
+pub fn geq(x: impl Into<LinSum>, y: impl Into<LinSum>) -> LinLeq {
+    x.into().geq(y)
 }
-pub fn lt(lhs: impl Into<LinSum>, rhs: impl Into<LinSum>) -> LinLeq {
-    lhs.into().lt(rhs)
+/// Requires one integer expression to be smaller than another one (`x < y`)
+pub fn lt(x: impl Into<LinSum>, y: impl Into<LinSum>) -> LinLeq {
+    x.into().lt(y)
 }
-pub fn gt(lhs: impl Into<LinSum>, rhs: impl Into<LinSum>) -> LinLeq {
-    lhs.into().gt(rhs)
+/// Requires one integer expression to be greater than another one (`x < y`)
+pub fn gt(x: impl Into<LinSum>, y: impl Into<LinSum>) -> LinLeq {
+    x.into().gt(y)
 }
 
+/// Requires that at least one literal is true (`b_1 || b_2 || ... || b_n`).
 pub fn or(disjuncts: impl Into<Disjunction>) -> Disjunction {
     disjuncts.into()
 }
+/// Requires that all literals are true (`b_1 && b_2 && ... && b_n`).
 pub fn and(conjuncts: impl Into<Conjunction>) -> Conjunction {
     conjuncts.into()
 }
+
+/// Requires that if `a` holds that `b` holds as well (`a => b`).
 pub fn implies(a: impl Into<Lit>, b: impl Into<Lit>) -> Disjunction {
     or([!a.into(), b.into()])
 }
@@ -83,11 +93,11 @@ where
     eq_max(-lhs, rhs.into_iter().map(|e| -e))
 }
 
-/// Transforms a boolean into an integer expression where `1` represents `true` and `0` represents false.
+/// Transforms a boolean into an integer expression where `1` represents `true` and `0` represents `false`.
 ///
-/// In most cases, this will reuse the variable underlying the literal but it may in some case require the introduction
+/// In most cases, this will reuse the variable underlying the literal but it may require the introduction
 /// of an auxiliary variable.
-pub fn bool2int<Ctx: ModelView + Dom>(b: Lit, model: &mut Ctx) -> LinTerm {
+pub fn bool2int<Ctx: ModelView>(b: Lit, model: &mut Ctx) -> LinTerm {
     let is_zero_one = model.bounds(b.variable()) == (0, 1);
     if model.entails(b) {
         1.into()
