be75eeba19fe44458727d361bba6121b9b9ac7a1
Nika Beriachvili <nikaberiachvilipro@gmail.com>
2026-08-04 11:16:17 +0200

    chore(lprelax): fix casting to and from FloatCst

--- solver/lprelax/src/types.rs
+++ solver/lprelax/src/types.rs
@@ -8,31 +8,22 @@ pub type LpModel = highs::Model;
 pub type LpObjectiveSense = highs::Sense;
 
 pub type FloatCst = f64;
+
 pub fn float_as_exact_int_cst(value: FloatCst) -> IntCst {
-    if value <= INT_CST_MIN.into() {
-        INT_CST_MIN
-    } else if value >= INT_CST_MAX.into() {
-        INT_CST_MAX
-    } else {
-        assert!(value.fract().abs() < 1e-6);
-        value as IntCst
-    }
+    debug_assert!(value.fract().abs() < 1e-6);
+    let v = (value.clamp(INT_CST_MIN as FloatCst, INT_CST_MAX as FloatCst) as IntCst).clamp(INT_CST_MIN, INT_CST_MAX);
+    debug_assert!((value - v as FloatCst).abs() < 1e-6);
+    v
 }
 pub fn float_as_floor_int_cst(value: FloatCst) -> IntCst {
-    if value <= INT_CST_MIN.into() {
-        INT_CST_MIN
-    } else if value >= INT_CST_MAX.into() {
-        INT_CST_MAX
-    } else {
-        value.floor() as IntCst
-    }
+    float_as_exact_int_cst(value.floor())
 }
 pub fn float_as_ceil_int_cst(value: FloatCst) -> IntCst {
-    if value <= INT_CST_MIN.into() {
-        INT_CST_MIN
-    } else if value >= INT_CST_MAX.into() {
-        INT_CST_MAX
-    } else {
-        value.ceil() as IntCst
-    }
+    float_as_exact_int_cst(value.ceil())
+}
+
+pub fn int_cst_as_float(value: IntCst) -> FloatCst {
+    let v = value as FloatCst;
+    debug_assert!(float_as_exact_int_cst(v) == value);
+    v
 }
